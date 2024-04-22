23544cc8fa084b0bb832167d57aa3f66bebf8670
이희웅 <dlgmldnd017@naver.com>
2024-04-22 22:44:20 +0900

    [FE] feat #3: 로그인 UI 구현

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     extend: {
       backgroundImage: {
         home: 'url(@/assets/background_home.png)',
+        login: 'url(@/assets/background_login.png)',
+      },
+
+      keyframes: {
+        //우리의 fall 애니메이션 keyframes
+        fall: {
+          '0%': { transform: 'translate(0%,-150%)' },
+          '50%': { transform: 'translate(0%,0%)' },
+        },
       },
     },
   },
