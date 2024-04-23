8cc3b1eea04bf46c818486d41b9960811bb396f3
이희웅 <dlgmldnd017@naver.com>
2024-04-23 12:47:16 +0900

    [FE] feat #3: 카카오 리다이렉트 UI 구현

--- frontend/src/components/login/KakaoLogin.tsx
+++ frontend/src/components/login/KakaoLogin.tsx
@@ -2,12 +2,20 @@
 import kakao from '@/assets/kakao.png';
 
 const KakaoLogin = () => {
-  // const { loginUser } = userStore();
+  /* .env 저장 필요 */
+  const REST_API_KEY = 'f5852906b761ebfba2853e0e1e3af9fd';
+  const REDIRECT_URI = 'http://localhost:5173/login/redirect/kakao';
+
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const loginHandler = () => {
+    window.location.href = link;
+  };
 
   return (
-    <button className="bg-white/40 flex items-center p-3 pl-7 rounded-xl">
+    <button className="bg-white/40 flex items-center p-3 pl-7 rounded-xl" onClick={loginHandler}>
       <img src={kakao}></img>
-      <div className="absolute translate-x-1/2 pl-1 text-lg font-bold">
+      <div className="absolute translate-x-1/2 text-lg font-bold">
         <p>카카오로 시작하기</p>
       </div>
     </button>
