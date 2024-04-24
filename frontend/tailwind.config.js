c06d91e0be8bc74ef548b206759275bd626da373
이승현 <sh980812@naver.com>
2024-04-24 15:43:55 +0900

    [FE] feat #6: bottomSheet 공통 컴포넌트 구현

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,20 @@ export default {
       backgroundImage: {
         home: 'url(@/assets/background_home.png)',
       },
+      keyframes: {
+        sheetOn: {
+          from: { opacity: 0, transform: 'translateY(384px)' },
+          to: { opacity: 1 },
+        },
+        sheetOff: {
+          from: { opacity: 1 },
+          to: { opacity: 0, transform: 'translateY(384px)' },
+        },
+      },
+      animation: {
+        sheetOn: 'sheetOn 0.3s ease-in-out',
+        sheetOff: 'sheetOff 0.3s ease-in',
+      },
     },
   },
   plugins: [],
