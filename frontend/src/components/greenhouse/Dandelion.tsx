7f1c991426ed6942dbf165802409b381e5a7b833
윤희원 <gmldnjs427@gmail.com>
2024-04-24 09:51:14 +0900

    [FE] design #4: 온실 배경, 민들레 위치 수정

--- frontend/src/components/greenhouse/Dandelion.tsx
+++ frontend/src/components/greenhouse/Dandelion.tsx
@@ -6,7 +6,7 @@ const Dandelion = ({ state }: IDandelionState) => {
   return (
     <img
       src={`src/assets/dandelion_${state % 6}.png`}
-      className={`absolute top-[390px] left-1/2 -translate-x-1/2 w-2/4`}
+      className={`absolute bottom-[20%] left-1/2 -translate-x-1/2 w-2/4`}
     ></img>
   );
 };
