2bdf999cfa96a5bc37219bf21cb630bb10a76924
이승현 <sh980812@naver.com>
2024-04-24 16:16:37 +0900

    [FE] feat #6: 메인 페이지 bottomSheet 구현
    펭귄 꾸미기 또는 상태 선택 구현

--- frontend/src/components/home/HomeHeader.tsx
+++ frontend/src/components/home/HomeHeader.tsx
@@ -3,7 +3,7 @@ import TransparentButton from '@/components/@common/TransparentButton';
 const HomeHeader = () => {
   return (
     <header>
-      <nav className="pr-5 pt-5 flex justify-end gap-3">
+      <nav className="p-5 flex justify-end gap-3">
         <TransparentButton text="광장" rounded="rounded-full" shadow="shadow-xl" onClick={() => console.log('광장')} />
         <TransparentButton
           text="아띠함"
--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -1,11 +1,18 @@
-import penguin from '@/assets/penguin.gif';
+import Penguin from '@/components/@common/Penguin';
 import HomeHeader from '@/components/home/HomeHeader';
+import PenguinDeco from '@/components/home/PenguinDeco';
+import { useState } from 'react';
 
 const HomePage = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
     <div className="relative w-full h-full bg-home bg-cover">
-      <img src={penguin} className="absolute left-0 right-0 bottom-28 " />
+      <div className="absolute left-0 right-0 bottom-[15%]">
+        <Penguin mode="" onClick={() => setIsOpen(!isOpen)} />
+      </div>
       <HomeHeader />
+      {isOpen && <PenguinDeco isOpen={isOpen} setIsOpen={setIsOpen} />}
     </div>
   );
 };
