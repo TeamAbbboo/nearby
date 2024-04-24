b53e7a149b835886696287ac8b2d1f09b5319083
이희웅 <dlgmldnd017@naver.com>
2024-04-24 09:14:18 +0900

    [FE] refactor #3: KakaoLogin 버튼을 TransparentButton으로 변경

--- frontend/src/components/login/KakaoLogin.tsx
+++ frontend/src/components/login/KakaoLogin.tsx
@@ -1,5 +1,6 @@
 /* Components */
 import kakao from '@/assets/kakao.png';
+import TransparentButton from '@/components/@common/TransparentButton';
 
 const KakaoLogin = () => {
   /* .env 저장 필요 */
@@ -13,12 +14,14 @@ const KakaoLogin = () => {
   };
 
   return (
-    <button className="bg-white/40 flex items-center p-3 pl-7 rounded-xl" onClick={loginHandler}>
-      <img src={kakao}></img>
-      <div className="absolute translate-x-1/2 text-lg font-bold">
-        <p>카카오로 시작하기</p>
+    <TransparentButton width="w-full" height="h-20" rounded="rounded-2xl" shadow="shadow-xl" onClick={loginHandler}>
+      <div className="flex items-center p-3 pl-7">
+        <img src={kakao}></img>
+        <div className="absolute translate-x-1/2 text-lg font-bold">
+          <p>카카오로 시작하기</p>
+        </div>
       </div>
-    </button>
+    </TransparentButton>
   );
 };
 
