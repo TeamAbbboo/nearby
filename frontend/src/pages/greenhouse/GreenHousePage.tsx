25a6f56da2d8bd169a3bcacffef723f8a97966b2
윤희원 <gmldnjs427@gmail.com>
2024-04-24 09:51:14 +0900

    [FE] design #4: 온실 배경, 민들레 위치 수정

--- frontend/src/pages/greenhouse/GreenHousePage.tsx
+++ frontend/src/pages/greenhouse/GreenHousePage.tsx
@@ -1,9 +1,16 @@
 import Dandelion from '@/components/greenhouse/Dandelion';
+import GreenhouseHeader from '@/components/greenhouse/GreenhouseHeader';
 
 const GreenHousePage = () => {
-  const state = 5;
+  const greenhouseProps = {
+    level: 4,
+    exp: 20,
+    expMax: 32,
+  };
 
   return (
+    <div className=" relative w-full h-full bg-GREENHOUSE bg-cover bg-center">
+      <Dandelion state={greenhouseProps.level} />
     <div className=" relative w-full h-full bg-GREENHOUSE bg-cover">
       <Dandelion state={state} />
     </div>
