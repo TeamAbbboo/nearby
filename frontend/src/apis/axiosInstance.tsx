05c50581e8637b3844b56888a40a48d69ff9ba24
이승현 <sh980812@naver.com>
2024-04-25 17:29:47 +0900

    [FE] feat #13: axios interceptors 구현

--- frontend/src/apis/axiosInstance.tsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const axiosRequestConfig = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-});
-
-export const axiosCommonInstance = axios.create(axiosRequestConfig);
