316a3f4199b977185d065a033e0742e7facb3d44
이승현 <sh980812@naver.com>
2024-04-26 10:28:22 +0900

    [FE] feat #13: 보낸 메시지 api mock 구현

--- frontend/src/mocks/api/handlers/messageHandlers.ts
+++ frontend/src/mocks/api/handlers/messageHandlers.ts
@@ -1,7 +1,7 @@
 import { HttpResponse, http } from 'msw';
 import { messageListRes } from '../data/message';
 
-export const authHandlers = [
+export const messageHandlers = [
   http.get('/messages/received', () => {
     return HttpResponse.json(messageListRes, { status: 201 });
   }),
