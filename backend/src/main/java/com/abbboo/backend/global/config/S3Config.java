b4cf46ce527e094574442ba48814d34462f3cd88
류지윤 <jiyun3016@gmail.com>
2024-04-30 01:44:47 +0900

    [BE] feat #36: S3 의존성 추가 및 S3 연결

---
+++ backend/src/main/java/com/abbboo/backend/global/config/S3Config.java
@@ -0,0 +1,33 @@
+package com.abbboo.backend.global.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class S3Config {
+
+    @Value("${cloud.aws.credentials.access-key")  // IAM 사용자 액세스 키
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key")  // IAM 사용자 비밀 액세스 키
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static")  // 버킷 리전
+    private String region;
+
+    // AmazonS3Client에 IAM 사용자 액세스 키, 버킷 리전 설정
+    @Bean
+    public AmazonS3Client amazonS3Client() {
+        BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+            .withCredentials(new AWSStaticCredentialsProvider(credentials))
+            .withRegion(region)
+            .build();
+    }
+
+}
