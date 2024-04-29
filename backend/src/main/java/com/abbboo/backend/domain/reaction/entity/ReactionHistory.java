6cffbf8cdfebbc8a9fff06b06269091881468317
류지윤 <jiyun3016@gmail.com>
2024-04-29 17:11:43 +0900

    [BE] feat #18: ReactionHistory entity 추가

---
+++ backend/src/main/java/com/abbboo/backend/domain/reaction/entity/ReactionHistory.java
@@ -0,0 +1,43 @@
+package com.abbboo.backend.domain.reaction.entity;
+
+import com.abbboo.backend.domain.story.entity.Story;
+import com.abbboo.backend.domain.user.entity.User;
+import com.abbboo.backend.global.base.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "reaction_history")
+public class ReactionHistory extends BaseEntity { // 사용자 반응 내역
+
+    @Column(name = "reaction_history_id")
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 반응이 달린 소식
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_id")
+    private Story story;
+
+    // 반응한 사용자
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reaction_id")
+    private Reaction reaction;
+
+}
