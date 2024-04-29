39b684ffb4c2b182e08769bc4a10bc1033329d36
이승현 <sh980812@naver.com>
2024-04-29 17:49:19 +0900

    [FE] feat #30: 광장 페이지 UI 구현

--- frontend/src/components/@common/Penguin.tsx
+++ frontend/src/components/@common/Penguin.tsx
@@ -1,17 +1,24 @@
 import penguin from '@/assets/penguin.gif';
+import scaffolding from '@/assets/scaffolding.png';
 
 interface IPenguinProps {
   width?: string;
   mode: string;
   decoration?: string;
+  isScaffolding?: boolean;
   onClick?: () => void;
 }
 
-const Penguin = ({ width, mode, decoration, onClick }: IPenguinProps) => {
+const Penguin = ({ width, mode, decoration, isScaffolding = false, onClick }: IPenguinProps) => {
   console.log(mode);
   console.log(decoration);
 
-  return <img onClick={onClick} src={penguin} alt="penguin" className={`${width}`}></img>;
+  return (
+    <>
+      <img onClick={onClick} src={penguin} alt="penguin" className={`${width} z-10`}></img>
+      {isScaffolding && <img src={scaffolding} alt="scaffolding" className="absolute bottom-0" />}
+    </>
+  );
 };
 
 export default Penguin;
--- frontend/src/pages/playground/PlaygroundPage.tsx
+++ frontend/src/pages/playground/PlaygroundPage.tsx
@@ -1,5 +1,17 @@
+import Penguin from '@/components/@common/Penguin';
+import FamilyInfo from '@/components/playground/FamilyInfo';
+import PlaygroundHeader from '@/components/playground/PlaygroundHeader';
+
 const PlaygroundPage = () => {
-  return <div>광장입니다</div>;
+  return (
+    <div className="relative w-full h-full bg-LOGIN bg-cover">
+      <PlaygroundHeader />
+      <div className="absolute left-0 right-0 bottom-[25%] flex justify-center">
+        <Penguin mode="" width="w-[350px]" isScaffolding={true} />
+      </div>
+      <FamilyInfo />
+    </div>
+  );
 };
 
 export default PlaygroundPage;
