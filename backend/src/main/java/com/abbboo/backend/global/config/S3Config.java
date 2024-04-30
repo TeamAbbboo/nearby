6f56653f97bc58b717ee6e21f26209e274e559e4
류지윤 <jiyun3016@gmail.com>
2024-04-30 15:54:20 +0900

    [BE] feat #36: s3 폴더 yml파일에서 환경변수로 관리하기

--- backend/src/main/java/com/abbboo/backend/global/config/S3Config.java
+++ backend/src/main/java/com/abbboo/backend/global/config/S3Config.java
@@ -4,11 +4,16 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConfigurationProperties(prefix = "cloud.aws.s3")
 public class S3Config {
 
     @Value("${cloud.aws.s3.credentials.access-key}")  // IAM 사용자 액세스 키
@@ -20,6 +25,9 @@ public class S3Config {
     @Value("${cloud.aws.s3.bucket}")  // 버킷 리전
     private String region;
 
+    @Getter @Setter
+    private List<String> directories; // 기능 별 폴더 분류 yml에서 매핑
+
     // AmazonS3Client에 IAM 사용자 액세스 키, 버킷 리전 설정
     @Bean
     public AmazonS3 amazonS3Client() {
@@ -29,5 +37,4 @@ public class S3Config {
             .withRegion(region)
             .build();
     }
-
 }
