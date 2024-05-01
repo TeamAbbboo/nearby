3cfb9984feb7e53bb4019eb3a3e7272e99a412e2
신영한 <syhan7516@naver.com>
2024-05-02 01:14:43 +0900

    [BE] feat #42: OAuth2UserDto 생성

---
+++ backend/src/main/java/com/abbboo/backend/global/auth/OAuth2UserDto.java
@@ -0,0 +1,14 @@
+package com.abbboo.backend.global.auth;
+
+import lombok.Builder;
+
+public class OAuth2UserDto {
+
+    // 생성된 사용자 아아디
+    private String createdUserId;
+
+    @Builder
+    public OAuth2UserDto(String createdUserId) {
+        this.createdUserId = createdUserId;
+    }
+}
