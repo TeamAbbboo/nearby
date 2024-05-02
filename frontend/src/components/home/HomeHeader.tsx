6e93e9a03aa2164540c4f0ab1561ce2d1d52c405
이승현 <sh980812@naver.com>
2024-05-02 10:30:30 +0900

    [FE] refactor #45: 메뉴바 아이콘 변경

--- frontend/src/components/home/HomeHeader.tsx
+++ frontend/src/components/home/HomeHeader.tsx
@@ -1,7 +1,11 @@
-import TransparentButton from '@/components/@common/TransparentButton';
 import MessageModal from './MessageModal';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import playground from '@/assets/icons/playground.png';
+import message from '@/assets/icons/message.png';
+import setting from '@/assets/icons/setting.png';
+import camera from '@/assets/icons/camera.png';
+import notification from '@/assets/icons/notification.png';
 
 const HomeHeader = () => {
   const navigate = useNavigate();
@@ -9,26 +13,43 @@ const HomeHeader = () => {
 
   return (
     <header>
-      <nav className="p-5 flex justify-end gap-3">
-        <TransparentButton
-          text="광장"
-          rounded="rounded-full"
-          shadow="shadow-xl"
-          onClick={() => navigate('/playground')}
-        />
-        <TransparentButton
-          text="아띠함"
-          rounded="rounded-full"
-          shadow="shadow-xl"
-          onClick={() => setIsMessageModalOpen(true)}
-        />
-        <TransparentButton
-          text="스토리"
-          rounded="rounded-full"
-          shadow="shadow-xl"
-          onClick={() => console.log('스토리')}
-        />
-        <TransparentButton text="설정" rounded="rounded-full" shadow="shadow-xl" onClick={() => console.log('설정')} />
+      <nav className="p-5 flex justify-between font-bold">
+        {/* 왼쪽 네비바 */}
+        <div className="flex flex-col gap-3">
+          <div onClick={() => console.log('소식 등록하기')} className="flex flex-col items-center gap-1 ">
+            <img src={camera} width={44} />
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
+              <p className="text-[9px]">소식 등록</p>
+            </div>
+          </div>
+          <div onClick={() => console.log('알림 확인하기')} className="flex flex-col items-center gap-1">
+            <img src={notification} width={44} />
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
+              <p className="text-[9px]">알림</p>
+            </div>
+          </div>
+        </div>
+        {/* 오른쪽 네비바 */}
+        <div className="flex flex-col gap-3">
+          <div onClick={() => navigate('/playground')} className="flex flex-col items-center">
+            <img src={playground} width={44} />
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
+              <p className="text-[9px]">광장</p>
+            </div>
+          </div>
+          <div onClick={() => setIsMessageModalOpen(true)} className="flex flex-col items-center">
+            <img src={message} width={44} />
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
+              <p className="text-[9px]">마음함</p>
+            </div>
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <img src={setting} width={44} />
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
+              <p className="text-[9px]">설정</p>
+            </div>
+          </div>
+        </div>
       </nav>
       {isMessageModalOpen && <MessageModal setIsMessageModalOpen={setIsMessageModalOpen} />}
     </header>
