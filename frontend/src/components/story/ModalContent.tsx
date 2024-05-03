da0358a6ba7745d2d186e3213d0f2d9625851b03
윤희원 <gmldnjs427@naver.com>
2024-05-03 14:43:29 +0900

    [FE] feat #35: 소식 페이지 UI 구현

---
+++ frontend/src/components/story/ModalContent.tsx
@@ -0,0 +1,10 @@
+import { useModal } from './ModalContext';
+import Story from '@/components/story/Story';
+
+const ModalContent = () => {
+  const { isModalOpen } = useModal();
+
+  return <>{isModalOpen && <Story />}</>;
+};
+
+export default ModalContent;
