047a48f0af36e4a46e47c5117ce6be7be2c647e6
신영한 <syhan7516@naver.com>
2024-05-03 14:34:14 +0900

    [BE] feat #42: CORS 설정

--- backend/src/main/java/com/abbboo/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/abbboo/backend/global/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.abbboo.backend.global.auth.CustomOAuth2UserService;
 import com.abbboo.backend.global.auth.CustomSuccessHandler;
 import com.abbboo.backend.global.filter.JwtFilter;
 import com.abbboo.backend.global.util.JwtUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +14,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +30,9 @@ public class SecurityConfig {
 
     @Value("${spring.security.oauth2.redirect.url.endpoint}")
     private String sendRedirectUrl;
+
+    @Value("${spring.security.frontend-url}")
+    private String frontendUrl;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -80,6 +88,38 @@ public class SecurityConfig {
                         // STATELESS 설정
                         // -> JWT 통해 인증, 인가를 수행하기 때문에 세션 비연결성으로 설정
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // CORS 설정
+        http
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        // 프론트엔드 서버 주소 허용
+                        configuration.setAllowedOrigins(Collections.singletonList(frontendUrl));
+
+                        // 모든 요청 메서드 허용
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+
+                        // 브라우저가 리소스 요청을 할 때 쿠키, 인증과 관련된 HTTP 헤더 전송 허용
+                        configuration.setAllowCredentials(true);
+
+                        // 모든 요청 헤더 서버가 받아들일 수 있도록 허용
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+
+                        // 프리플라이트 요청의 결과를 캐싱할 시간 설정
+                        configuration.setMaxAge(3600L);
+
+                        // JWT 받을 수 있도록 허용
+                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
 
         return http.build();
     }
