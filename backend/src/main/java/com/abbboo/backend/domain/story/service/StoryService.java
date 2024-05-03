ed8f8069c143289e76b9c44bf73178fb78e879e7
류지윤 <jiyun3016@gmail.com>
2024-05-03 15:24:44 +0900

    [BE] feat #57: 소식 이미지 파일 등록 변경으로 API 수정

--- backend/src/main/java/com/abbboo/backend/domain/story/service/StoryService.java
+++ backend/src/main/java/com/abbboo/backend/domain/story/service/StoryService.java
@@ -6,7 +6,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface StoryService {
 
-    void createStroy(MultipartFile multipartFile);
+    void createStroy(MultipartFile frontFile, MultipartFile rearFile);
 
     void updateIsSaved(Long storyId);
 
