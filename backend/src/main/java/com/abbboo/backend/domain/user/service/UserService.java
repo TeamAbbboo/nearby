1a48a511b02310e42b273d80d4b2638467df341d
신영한 <syhan7516@naver.com>
2024-05-05 23:31:37 +0900

    [BE] feat #73: 로그인 비지니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/user/service/UserService.java
+++ backend/src/main/java/com/abbboo/backend/domain/user/service/UserService.java
@@ -1,9 +1,13 @@
 package com.abbboo.backend.domain.user.service;
 
 import com.abbboo.backend.domain.user.dto.req.UserModifyReq;
+import com.abbboo.backend.domain.user.dto.res.UserLoginRes;
 
 public interface UserService {
 
     // 유저 정보 수정
     void updateUser(String kakaoId, UserModifyReq userModifyReq);
+
+    // 유저 로그인
+    UserLoginRes getUserAll(String kakaoId);
 }
