a8cf672a545f69e6933695ce5430281841d611b4
신영한 <syhan7516@naver.com>
2024-05-05 23:31:59 +0900

    [BE] feat #73: 로그인 API 구현

--- backend/src/main/java/com/abbboo/backend/domain/user/controller/UserController.java
+++ backend/src/main/java/com/abbboo/backend/domain/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.abbboo.backend.domain.user.controller;
 
 import com.abbboo.backend.domain.user.dto.req.UserModifyReq;
+import com.abbboo.backend.domain.user.dto.res.UserLoginRes;
 import com.abbboo.backend.domain.user.service.UserService;
 import com.abbboo.backend.global.auth.CustomOAuth2User;
 import com.abbboo.backend.global.base.BaseResponse;
@@ -26,14 +27,28 @@ public class UserController {
     @PatchMapping("")
     public ResponseEntity<BaseResponse> modifyUser(
             @RequestBody UserModifyReq userModifyReq,
-            @AuthenticationPrincipal CustomOAuth2User customUserDetails) {
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
 
         log.info("유저 정보 수정 URL 맵핑 : OK");
 
         log.info("유저 정보 수정 : START");
-        userService.updateUser(customUserDetails.getCreatedUserId(),userModifyReq);
+        userService.updateUser(customOAuth2User.getCreatedUserId(),userModifyReq);
         log.info("유저 정보 수정 : COMPLETE");
 
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.USER_MODIFY_SUCCESS));
+    }
+
+    @Operation(summary = "유저 로그인")
+    @PostMapping("/login")
+    public ResponseEntity<BaseResponse> loginUser(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+
+        log.info("유저 로그인 URL 맵핑 : OK");
+
+        log.info("유저 로그인 : START");
+        UserLoginRes userLoginRes = userService.getUserAll(customOAuth2User.getCreatedUserId());
+        log.info("유저 로그인 : COMPLETE");
+
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.USER_LOGIN_SUCCESS,userLoginRes));
     }
 }
