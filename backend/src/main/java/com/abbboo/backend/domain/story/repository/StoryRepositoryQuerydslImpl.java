0d6c0a13805a3476e18a7ebc9a73f71d2d05045c
류지윤 <jiyun3016@gmail.com>
2024-05-07 05:16:35 +0900

    [BE] feat #85: 일별 소식 조회 구현 및 24시간 이내 소식 조회 일부 수정
    - 수정 이유 : 24시간이내 소식 조회와 일별 소식 조회에서 중복되는 코드를 공통 메서드로 분리.
    공통 메서드는 각 조회에 해당하는 조건을 인자로 받아야하므로 조건들 또한 같은 반환 타입을 생성 및 수정.
    - 수정 내용1 : 24시간 조회 메소드에서 소식 조회쿼리를 따로 메서드로 생성
    - 수정 내용2 : 위 소식 조회 쿼리 메서드가 들어오는 조건에 따라 조회되도록 수정
    - 수정 내용3 : 24시간 조회에 사용되던 조건 BooleanBuilder -> BooleamExpression

--- backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydslImpl.java
+++ backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydslImpl.java
@@ -3,13 +3,17 @@ package com.abbboo.backend.domain.story.repository;
 import static com.abbboo.backend.domain.reaction.entity.QReactionHistory.reactionHistory;
 import static com.abbboo.backend.domain.story.entity.QStory.story;
 
+import com.abbboo.backend.domain.story.dto.req.YearMonthDayParams;
 import com.abbboo.backend.domain.story.dto.res.DayStoryListRes;
 import com.abbboo.backend.domain.story.dto.res.DayStoryRes;
 import com.abbboo.backend.domain.story.dto.res.ReactionRes;
-import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -20,24 +24,33 @@ public class StoryRepositoryQuerydslImpl implements StoryRepositoryQuerydsl{
 
     private final JPAQueryFactory jpaQueryFactory;
 
+    // 24시간 이내의 가족들의 보관된 소식 조회
     @Override
     public DayStoryListRes findDayStoriesByFamilyId(int familyId) {
-        // where절 조건
-        BooleanBuilder booleanBuilder =generateQueryCondition(familyId);
+        return findStoryList(twentyfourHoursCondition(familyId));
+    }
 
-        // 24시간이내 가족들의 소식 조회
+    // 요청된 일자에 따른 가족들의 보관된 소식 조회
+    @Override
+    public DayStoryListRes findDailySavedStoriesByFamilyId(YearMonthDayParams params, int familyId) {
+        return findStoryList(YearMonthDayCondition(params, familyId));
+    }
+
+    // 조건에 따른 보관된 소식 조회
+    private DayStoryListRes findStoryList(BooleanExpression condition){
+
         List<DayStoryRes> dayStoryResList = jpaQueryFactory.select(Projections.fields(
-            DayStoryRes.class,
-            story.id.as("storyId"),
-            story.user.id.as("userId"),
-            story.frontUrl.as("frontUrl"),
-            story.rearUrl.as("rearUrl"),
-            story.user.mood.as("mood"),
-            story.user.nickname.as("nickname"),
-            story.isSaved.as("isSaved")
-        ))
+                DayStoryRes.class,
+                story.id.as("storyId"),
+                story.frontUrl.as("frontUrl"),
+                story.rearUrl.as("rearUrl"),
+                story.user.mood.as("mood"),
+                story.user.nickname.as("nickname"),
+                story.isSaved.as("isSaved"),
+                story.createdAt.as("createdAt")
+            ))
             .from(story)
-            .where(booleanBuilder)
+            .where(condition)
             .orderBy(story.createdAt.asc())
             .fetch();
 
@@ -51,10 +64,10 @@ public class StoryRepositoryQuerydslImpl implements StoryRepositoryQuerydsl{
     private void ReactionForDayStory(DayStoryRes dayStoryRes){
         List<ReactionRes> reactionRes = jpaQueryFactory.select(Projections.fields(
             ReactionRes.class,
-            reactionHistory.user.id.as("userId"),
             reactionHistory.user.mood,
             reactionHistory.user.nickname,
-            reactionHistory.reaction.expression
+            reactionHistory.reaction.expression,
+            reactionHistory.createdAt
         ))
             .from(reactionHistory)
             .where(reactionHistory.story.id.eq(dayStoryRes.getStoryId()))    // 각 소식 id
@@ -63,13 +76,18 @@ public class StoryRepositoryQuerydslImpl implements StoryRepositoryQuerydsl{
         dayStoryRes.setReactions(reactionRes);
     }
 
-    private BooleanBuilder generateQueryCondition(int familyId){
-        BooleanBuilder booleanBuilder = new BooleanBuilder();
+    // 비교 조건 : 가족 id 일치 & 생성일자가 지금으로부터 24시간 이내인 조건 생성
+    private BooleanExpression twentyfourHoursCondition(int familyId){
 
-        // 지금으로부터 24시간 이내인 소식을 조회하는 조건
         LocalDateTime dayAgo = LocalDateTime.now().minusHours(24);
-        booleanBuilder.and(story.user.family.id.eq(familyId))
-            .and(story.createdAt.after(dayAgo));
-        return booleanBuilder;
+        return (story.user.family.id.eq(familyId)).and(story.createdAt.after(dayAgo));
+    }
+
+    // 동등 조건 : 생성일자가 요청된 일자(YYYY-MM-dd)와 일치하는 조건 생성
+    private BooleanExpression YearMonthDayCondition(YearMonthDayParams params,int familyId){
+        YearMonth yearMonth = YearMonth.of(params.getYear(), params.getMonth());
+        LocalDate requestDay = yearMonth.atDay(params.getDay());
+        return (Expressions.dateTemplate(LocalDate.class, "DATE({0})", story.createdAt)
+            .eq(requestDay)).and(story.user.family.id.eq(familyId));
     }
 }
