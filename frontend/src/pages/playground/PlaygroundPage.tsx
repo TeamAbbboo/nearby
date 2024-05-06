74cc85711106380d1e80028061a64e24de349025
이승현 <sh980812@naver.com>
2024-05-06 23:27:49 +0900

    [FE] feat #81: 가족 정보 및 상태 조회 데이터 통신 구현

--- frontend/src/pages/playground/PlaygroundPage.tsx
+++ frontend/src/pages/playground/PlaygroundPage.tsx
@@ -1,15 +1,25 @@
 import Penguin from '@/components/@common/Penguin';
 import FamilyInfo from '@/components/playground/FamilyInfo';
 import PlaygroundHeader from '@/components/playground/PlaygroundHeader';
+import { usePlayground } from '@/hooks/playground/usePlayground';
 
 const PlaygroundPage = () => {
+  const { useGetFamilyInfoList } = usePlayground();
+  const { data: familyInfo } = useGetFamilyInfoList();
+
+  console.log(familyInfo);
+
   return (
-    <div className="relative w-full h-full bg-LOGIN bg-cover">
-      <PlaygroundHeader />
-      <div className="absolute left-0 right-0 bottom-[25%] flex justify-center">
-        <Penguin mode="" width="w-[350px]" isScaffolding={true} />
+    <div className={`relative w-full h-screen`}>
+      <div className="w-full h-full bg-LOGIN bg-cover">
+        <PlaygroundHeader />
+        <div className="absolute w-full flex overflow-x-auto bottom-[30%] justify-center">
+          <div className="w-[150px]">
+            <Penguin mode="" />
+          </div>
+        </div>
+        <FamilyInfo />
       </div>
-      <FamilyInfo />
     </div>
   );
 };
