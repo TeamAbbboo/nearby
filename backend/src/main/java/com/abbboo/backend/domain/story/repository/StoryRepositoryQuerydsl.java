81a5e7d0c90eb2e686029d86e046bd2fcf254165
류지윤 <jiyun3016@gmail.com>
2024-05-07 00:10:49 +0900

    [BE] feat #55: 월 별 소식 조회 querydsl 구현

--- backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydsl.java
+++ backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydsl.java
@@ -1,8 +1,13 @@
 package com.abbboo.backend.domain.story.repository;
 
+import com.abbboo.backend.domain.story.dto.req.MonthlyStoriesParams;
 import com.abbboo.backend.domain.story.dto.res.DayStoryListRes;
+import com.abbboo.backend.domain.story.dto.res.MonthlyStoryRes;
+import java.util.List;
 
 public interface StoryRepositoryQuerydsl {
 
     DayStoryListRes findDayStoriesByFamilyId(int familyId);
+
+    List<MonthlyStoryRes> findMonthlyStories(MonthlyStoriesParams monthlyStoriesParams, int familyId);
 }
