0dd38451444ead606dca5a4dffc9eb7ab4277c9c
류지윤 <jiyun3016@gmail.com>
2024-05-07 05:09:21 +0900

    [BE] feat #85: 일별 소식 조회 비즈니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydsl.java
+++ backend/src/main/java/com/abbboo/backend/domain/story/repository/StoryRepositoryQuerydsl.java
@@ -1,8 +1,11 @@
 package com.abbboo.backend.domain.story.repository;
 
+import com.abbboo.backend.domain.story.dto.req.YearMonthDayParams;
 import com.abbboo.backend.domain.story.dto.res.DayStoryListRes;
 
 public interface StoryRepositoryQuerydsl {
 
     DayStoryListRes findDayStoriesByFamilyId(int familyId);
+
+    DayStoryListRes findDailySavedStoriesByFamilyId(YearMonthDayParams params, int familyId);
 }
