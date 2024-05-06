588fb89c930c03bd2ead481e3c486ef1bfe5e118
이승현 <sh980812@naver.com>
2024-05-06 20:41:59 +0900

    [FE] design: home page UI 수정

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -2,13 +2,16 @@ import Penguin from '@/components/@common/Penguin';
 import HomeHeader from '@/components/home/HomeHeader';
 import PenguinDecoBottomSheet from '@/components/home/PenguinBottomSheet';
 import { useState } from 'react';
+import home from '@/assets/background_home.png';
+
 const HomePage = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   return (
-    <div className="relative w-full h-full bg-HOME bg-cover">
+    <div className="relative w-full h-full">
+      <img src={home} className="w-full h-full" />
       <div className="absolute left-0 right-0 bottom-[15%] flex justify-center">
-        <Penguin mode="" onClick={() => setIsOpen(!isOpen)} width="w-[350px]" />
+        <Penguin mode="" onClick={() => setIsOpen(!isOpen)} width="w-4/5" />
       </div>
       <HomeHeader />
       {isOpen && <PenguinDecoBottomSheet isOpen={isOpen} setIsOpen={setIsOpen} />}
