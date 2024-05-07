fec7fe4572de144fec9c9fd9c9ae6b02843858f9
류지윤 <jiyun3016@gmail.com>
2024-05-08 03:56:52 +0900

    [BE] feat #100: 보낸 메시지 조회 API 구현

--- backend/src/main/java/com/abbboo/backend/domain/message/controller/MessageController.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/controller/MessageController.java
@@ -1,23 +1,28 @@
 package com.abbboo.backend.domain.message.controller;
 
-import static com.abbboo.backend.global.base.SuccessCode.MESSAGE_SEND_SUCCESS;
-
 import com.abbboo.backend.domain.message.dto.req.SendMessageReq;
+import com.abbboo.backend.domain.message.dto.res.SentMessageRes;
 import com.abbboo.backend.domain.message.service.MessageService;
 import com.abbboo.backend.global.auth.CustomOAuth2User;
-import com.abbboo.backend.global.base.BaseEntity;
 import com.abbboo.backend.global.base.BaseResponse;
+import com.abbboo.backend.global.base.PagenationReq;
 import com.abbboo.backend.global.base.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/messages")
 @RequiredArgsConstructor
@@ -34,6 +39,16 @@ public class MessageController {
 
         messageService.createMessage(customOAuth2User.getCreatedUserId(), req);
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.MESSAGE_SEND_SUCCESS));
+    }
+
+    @Operation(summary = "가족들에게 보낸 메시지 조회 API")
+    @GetMapping("/sent")
+    public ResponseEntity<BaseResponse> getSentMessage(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+        @ModelAttribute @ParameterObject PagenationReq pagenationReq){
+
+        Slice<SentMessageRes> sentMessages =  messageService.findSentMessage(customOAuth2User.getCreatedUserId(), pagenationReq);
+        log.info("가족들에게 보낸 메시지 조회 성공 : {}", sentMessages);
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.GET_SENT_MESSAGE_SUCCESS, sentMessages));
     }
 
 }
