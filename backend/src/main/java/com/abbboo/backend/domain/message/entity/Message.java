5a52678e8374f09a19de1fce7f47f1cb22ff86bc
류지윤 <jiyun3016@gmail.com>
2024-05-07 17:56:01 +0900

    [BE] feat #61: 데이터 요청과 응답에 필요한 객체 수정 및 생성

--- backend/src/main/java/com/abbboo/backend/domain/message/entity/Message.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/entity/Message.java
@@ -12,12 +12,18 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
 
 @Entity
 @Getter
+@Builder
+@DynamicInsert
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "message")
 public class Message extends BaseEntity { // 쪽지
