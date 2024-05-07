e5b63083c5e615cc4e6496ddcbedf0291bb89ca5
신영한 <syhan7516@naver.com>
2024-05-07 22:12:50 +0900

    [BE] feat #91: 가족 코드 조회 비지니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/family/service/FamilyService.java
+++ backend/src/main/java/com/abbboo/backend/domain/family/service/FamilyService.java
@@ -1,9 +1,13 @@
 package com.abbboo.backend.domain.family.service;
 
 import com.abbboo.backend.domain.family.dto.res.FamilyGenerateRes;
+import com.abbboo.backend.domain.family.dto.res.FamilyCodeCheckRes;
 
 public interface FamilyService {
 
     // 가족 생성
     FamilyGenerateRes createFamily(String kakaoId);
+
+    // 가족 코드 조회
+    FamilyCodeCheckRes getFamilyCode(String kakaoId);
 }
