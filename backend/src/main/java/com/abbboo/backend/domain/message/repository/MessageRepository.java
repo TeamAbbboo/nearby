e0c14ea790d1f9f0bc7508c85da6c4a18890954d
류지윤 <jiyun3016@gmail.com>
2024-05-08 03:58:57 +0900

    [BE] feat #100: 가족들에게 보낸 메시지 조회 비즈니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
@@ -1,8 +1,19 @@
 package com.abbboo.backend.domain.message.repository;
 
+import com.abbboo.backend.domain.message.dto.res.SentMessageRes;
 import com.abbboo.backend.domain.message.entity.Message;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
+    @Query("select new com.abbboo.backend.domain.message.dto.res.SentMessageRes(" +
+            "u.id, u.nickname, u.mood, m.content, m.createdAt)" +
+            "from User u join Message m on u.id = m.receiver.id " +
+            "where m.sender.id= :senderId and u.family.id= :familyId order by m.createdAt")
+    Slice<SentMessageRes> findSentMessage(@Param("senderId") int senderId,
+        @Param("familyId") int familyId, PageRequest pageRequest);
 }
