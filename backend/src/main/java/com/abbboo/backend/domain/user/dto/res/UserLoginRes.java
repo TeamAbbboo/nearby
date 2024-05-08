032a9000150541292d570ff34c44b2026162619f
신영한 <syhan7516@naver.com>
2024-05-08 17:11:46 +0900

    [BE] fix #119: 로그인 응답에 필요한 Dto 수정
    수정 내용 : 유저의 모든 정보 반환 -> 유저가 속한 가족 ID 반환
    수정 이유 : 최소한의 데이터 반환을 위해 수정

--- backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserLoginRes.java
+++ backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserLoginRes.java
@@ -1,4 +1,5 @@
 package com.abbboo.backend.domain.user.dto.res;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -6,26 +7,11 @@ import java.time.LocalDate;
 @Getter
 public class UserLoginRes {
 
-    // 유저 식별 ID
-    private Integer userId;
-
     // 가족 식별 ID
     private Integer familyId;
 
-    // 유저 닉네임
-    private String nickname;
-
-    // 유저 생일
-    private LocalDate birthday;
-
-    // 유저 상태
-    private String mood;
-
-    public UserLoginRes(Integer userId, Integer familyId, String nickname, LocalDate birthday, String mood) {
-        this.userId = userId;
+    @Builder
+    public UserLoginRes(Integer familyId) {
         this.familyId = familyId;
-        this.nickname = nickname;
-        this.birthday = birthday;
-        this.mood = mood;
     }
 }
