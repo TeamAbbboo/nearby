95934d66d282bf790746aa89f5eab486a5af16b8
류지윤 <jiyun3016@gmail.com>
2024-05-08 09:51:03 +0900

    [BE] feat #100: 중복된 order by 삭제

--- backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
@@ -13,7 +13,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     @Query("select new com.abbboo.backend.domain.message.dto.res.SentMessageRes(" +
             "u.id, u.nickname, u.mood, m.content, m.createdAt)" +
             "from User u join Message m on u.id = m.receiver.id " +
-            "where m.sender.id= :senderId and u.family.id= :familyId order by m.createdAt")
+            "where m.sender.id= :senderId and u.family.id= :familyId")
     Slice<SentMessageRes> findSentMessage(@Param("senderId") int senderId,
         @Param("familyId") int familyId, PageRequest pageRequest);
 }
