bf1c4400cb711d7f0d354ab238c23d5e62096b16
류지윤 <jiyun3016@gmail.com>
2024-05-08 20:10:54 +0900

    [BE] feat #115: 메시지 읽음 처리 API 구현

--- backend/src/main/java/com/abbboo/backend/domain/message/controller/MessageController.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/controller/MessageController.java
@@ -19,6 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -76,5 +78,16 @@ public class MessageController {
             customOAuth2User.getCreatedUserId());
         log.info("안 읽은 메시지 중 가장 최근 메시지 조회 성공!!");
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.GET_RECEIVED_MESSAGE_SUCCESS, unreadMessageRes));
+    }
+
+    @Operation(summary = "메시지 읽음 처리 API")
+    @PatchMapping
+    public ResponseEntity<BaseResponse> getReadMessageOne(
+        @RequestBody Long messageId,
+        @AuthenticationPrincipal CustomOAuth2User customOAuth2User){
+
+        log.info("id : {} 메시지 읽기에 성공!!", messageId);
+        messageService.updateMessageIsRead(customOAuth2User.getCreatedUserId(), messageId);
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.READ_MESSAGE_SUCCESS));
     }
 }
