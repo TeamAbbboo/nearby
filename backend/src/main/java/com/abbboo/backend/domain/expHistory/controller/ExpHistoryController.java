a2472cf878edd295aab10e1c26325d59ef80f163
김예현 <qkdo12@naver.com>
2024-05-08 15:42:26 +0900

    [BE] refactor #107: 가족관련 에러 코드 수정, Pagenation 에러처리

--- backend/src/main/java/com/abbboo/backend/domain/expHistory/controller/ExpHistoryController.java
+++ backend/src/main/java/com/abbboo/backend/domain/expHistory/controller/ExpHistoryController.java
@@ -6,8 +6,11 @@ import com.abbboo.backend.global.auth.CustomOAuth2User;
 import com.abbboo.backend.global.base.BaseResponse;
 import com.abbboo.backend.global.base.PagenationReq;
 import com.abbboo.backend.global.base.SuccessCode;
+import com.abbboo.backend.global.error.ErrorCode;
+import com.abbboo.backend.global.error.exception.NotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springdoc.core.annotations.ParameterObject;
@@ -25,10 +28,19 @@ public class ExpHistoryController {
     final private ExpHistoryService expHistoryService;
     @GetMapping
     @Operation(summary = "경험치 내역 조회")
+
     ResponseEntity<BaseResponse> getExpHistories(@AuthenticationPrincipal CustomOAuth2User customOAuth2User,
                                                  @ModelAttribute @ParameterObject PagenationReq pagenationReq)
     {
         log.info("userId : {}",customOAuth2User.getCreatedUserId());
+        if(pagenationReq.getPage()<0)
+        {
+            throw new NotFoundException(ErrorCode.INVALID_PAGE_NUMBER);
+        }
+        if(pagenationReq.getSize()<1)
+        {
+            throw new NotFoundException(ErrorCode.INVALID_PAGE_SIZE);
+        }
         GetExpHistoryRes getExpHistoryRes=expHistoryService.getExpHistory(customOAuth2User.getCreatedUserId(),pagenationReq);
         return ResponseEntity.ok(BaseResponse.of(SuccessCode.EXPHISTORY_GET_SUCCESS,getExpHistoryRes));
     }
