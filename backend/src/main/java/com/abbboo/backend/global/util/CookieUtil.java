f2521cd95d800f0575c29a6875f62f54211fa829
신영한 <syhan7516@naver.com>
2024-05-10 00:23:27 +0900

    [BE] fix #128: 쿠키와 연관된 메서드들 이동
    - 변경 내용 : JwtUtil (쿠키 확인), CustomSuccessHandler (쿠키 생성) -> CookieUtil 위치로 이동
    - 변경 이유 : 여러 곳에서 사용되는 것을 확인하여 연관된 메서드들을 하나의 클래스로 관리하기 위해 위치 변경

--- backend/src/main/java/com/abbboo/backend/global/util/CookieUtil.java
+++ backend/src/main/java/com/abbboo/backend/global/util/CookieUtil.java
@@ -1,6 +1,7 @@
 package com.abbboo.backend.global.util;
 
 import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -27,5 +28,28 @@ public class CookieUtil {
         cookie.setHttpOnly(true);
 
         return cookie;
+    }
+
+    // 쿠키에서 토큰 확인 메서드
+    public String getCookieToken(HttpServletRequest request) {
+
+        log.info("토큰 재발급 요청 : OK");
+
+        // 쿠기 가져오기
+        Cookie[] cookies = request.getCookies();
+
+        // 쿠키가 존재할 경우
+        if(cookies != null) {
+            for(Cookie cookie : cookies) {
+
+                // 쿠키 키가 refreshToken 경우
+                if (cookie.getName().equals("refreshToken")) {
+                    request.setAttribute("refreshToken",cookie.getValue());
+                    return cookie.getValue();
+                }
+            }
+        }
+
+        return null;
     }
 }
