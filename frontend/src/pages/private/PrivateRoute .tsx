83d6122330e50019f5d2d1990c647ecd59a2edf7
이희웅 <dlgmldnd017@naver.com>
2024-05-09 13:32:45 +0900

    [FE] fix : 로그인 페이지에서 ACCESS_TOKEN 비교 로직 수정

--- frontend/src/pages/private/PrivateRoute .tsx
+++ frontend/src/pages/private/PrivateRoute .tsx
@@ -3,9 +3,7 @@ import { Navigate } from 'react-router-dom';
 const PrivateRoute = ({ children }: { children: React.ReactNode }) => {
   if (
     localStorage.getItem('ACCESS_TOKEN') &&
-    localStorage
-      .getItem('ACCESS_TOKEN')
-      ?.startsWith('eyJhbGciOiJIUzI1NiJ9.eyJjcmVhdGVkVXNlcklkIjoia2FrYW8tMzQ1OTkyOTc3M')
+    localStorage.getItem('ACCESS_TOKEN')?.startsWith('eyJhbGciOiJIUzI1NiJ9')
   ) {
     return children;
   }
