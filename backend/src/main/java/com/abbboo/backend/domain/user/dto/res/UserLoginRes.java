16a5111de53863290e94729ee2d7c7caa7a0a1a9
신영한 <syhan7516@naver.com>
2024-05-09 14:52:33 +0900

    [BE] fix #133: 로그인 응답에 필요한 Dto 변경
    변경 내용 : 유저가 속한 가족 ID 반환 -> 유저의 닉네임, 생년월일, 가족 ID 반환
    변경 이유 : 로그인 이후 현재 유저의 데이터에 따른 페이지 접근 경로를 나누기 위해 변경

--- backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserLoginRes.java
+++ backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserLoginRes.java
@@ -10,8 +10,16 @@ public class UserLoginRes {
     // 가족 식별 ID
     private Integer familyId;
 
+    // 닉네임
+    private String nickname;
+
+    // 생년월일
+    private LocalDate birthday;
+
     @Builder
-    public UserLoginRes(Integer familyId) {
+    public UserLoginRes(Integer familyId, String nickname, LocalDate birthday) {
         this.familyId = familyId;
+        this.nickname = nickname;
+        this.birthday = birthday;
     }
 }
