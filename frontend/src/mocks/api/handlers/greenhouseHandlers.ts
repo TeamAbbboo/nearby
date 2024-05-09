e55837e92396df72f88d5dd4d8bcaae8df473a4f
윤희원 <gmldnjs427@naver.com>
2024-05-09 19:36:39 +0900

    [FE] refactor #66: msw mock 데이터 수정

--- frontend/src/mocks/api/handlers/greenhouseHandlers.ts
+++ frontend/src/mocks/api/handlers/greenhouseHandlers.ts
@@ -5,20 +5,12 @@ export const greenhouseHandlers = [
   http.get('/level', () => {
     return HttpResponse.json(getCurrentLevelReq, {
       status: 200,
-      headers: {
-        authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0bmdoazk2MTFAbmF2ZXIuY29tIiwiaWF0IjoxNzExOTMyNzA2LCJleHAiOjE3MTIwMTkxMDYsImF1dGgiOiJqYXZhLnV0aWwuc3RyZWFtLlJlZmVyZW5jZVBpcGVsaW5lJDNAMThjZmVmZjcifQ.EjNEBOOjYYFM_rGWUrDq7di7dVhHmaCto074s4l2GD8',
-      },
     });
   }),
 
   http.patch('/level', () => {
     const success = HttpResponse.json(patchLevelUpReq, {
       status: 200,
-      headers: {
-        authorization:
-          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0bmdoazk2MTFAbmF2ZXIuY29tIiwiaWF0IjoxNzExOTMyNzA2LCJleHAiOjE3MTIwMTkxMDYsImF1dGgiOiJqYXZhLnV0aWwuc3RyZWFtLlJlZmVyZW5jZVBpcGVsaW5lJDNAMThjZmVmZjcifQ.EjNEBOOjYYFM_rGWUrDq7di7dVhHmaCto074s4l2GD8',
-      },
     });
     return success;
   }),
