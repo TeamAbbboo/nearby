21d41d1728870afe1c0842c794381e6dedf72c98
이희웅 <dlgmldnd017@naver.com>
2024-05-09 10:11:39 +0900

    [FE] feat #60: 401 응답 에러 받으면 ACCESS_TOKEN 삭제 후 로그인 페이지로 이동

--- frontend/src/mocks/api/data/family.ts
+++ frontend/src/mocks/api/data/family.ts
@@ -3,8 +3,7 @@ export const getFamilyCodeRes = {
   message: '가족코드 조회를 성공하였습니다.',
   code: 'SUCCESS',
   data: {
-    // familyCode: 'ABBO01',
-    familyCode: '',
+    familyCode: 'ABBO01',
   },
 };
 
