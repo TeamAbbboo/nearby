48fc409d35659fa2612de260a121e7dddaf36ce3
이승현 <sh980812@naver.com>
2024-05-09 16:55:07 +0900

    [FE] design #138: 메인 및 광장 페이지 아이콘 수정

--- frontend/src/components/playground/PlaygroundHeader.tsx
+++ frontend/src/components/playground/PlaygroundHeader.tsx
@@ -13,13 +13,13 @@ const PlaygroundHeader = () => {
       <nav className="w-full p-5 flex justify-between font-bold">
         {/* 왼쪽 네비바 */}
         <div className="flex flex-col gap-3">
-          <div onClick={() => navigate('/story')} className="flex flex-col items-center gap-1 ">
+          <div onClick={() => navigate('/story')} className="flex flex-col items-center">
             <img src={camera} width={44} />
             <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
               <p className="text-[9px]">소식 등록</p>
             </div>
           </div>
-          <div onClick={() => console.log('알림 확인하기')} className="flex flex-col items-center gap-1">
+          <div onClick={() => console.log('알림 확인하기')} className="flex flex-col items-center">
             <img src={notification} width={44} />
             <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
               <p className="text-[9px]">알림</p>
@@ -30,13 +30,13 @@ const PlaygroundHeader = () => {
         <div className="flex flex-col gap-3">
           <div onClick={() => navigate('/')} className="flex flex-col items-center">
             <img src={home} width={44} />
-            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] mt-1 h-4 flex items-center justify-center">
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
               <p className="text-[9px]">홈</p>
             </div>
           </div>
           <div onClick={() => navigate('/greenhouse')} className="flex flex-col items-center">
             <img src={greenhouse} width={44} />
-            <div className="bg-black/60 text-white rounded-2xl text-center mt-1 w-[51px] h-4 flex items-center justify-center">
+            <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
               <p className="text-[9px]">온실</p>
             </div>
           </div>
