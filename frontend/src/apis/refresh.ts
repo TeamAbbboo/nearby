c8fc4339e533e616ef0e582ffd47147bf29d71a0
이희웅 <dlgmldnd017@naver.com>
2024-05-09 10:11:39 +0900

    [FE] feat #60: 401 응답 에러 받으면 ACCESS_TOKEN 삭제 후 로그인 페이지로 이동

--- frontend/src/apis/refresh.ts
+++ frontend/src/apis/refresh.ts
@@ -7,8 +7,9 @@ import { AxiosError } from 'axios';
 const refresh = async (error: AxiosError) => {
   // refresh 토큰이 만료되었을 경우 -> 로그아웃 처리
   if (error.response?.status === 401) {
-    localStorage.setItem('ACCESS_TOKEN', '');
+    localStorage.removeItem('ACCESS_TOKEN');
     window.location.replace('/login');
+    alert('로그아웃!!');
   }
 
   // access 토큰이 만료되었을 경우 -> 재발급 요청
