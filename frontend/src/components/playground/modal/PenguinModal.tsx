ed84b481bb7eefbfaca6bea876ff44db104bc785
이승현 <sh980812@naver.com>
2024-05-09 16:55:07 +0900

    [FE] design #138: 메인 및 광장 페이지 아이콘 수정

--- frontend/src/components/playground/modal/PenguinModal.tsx
+++ frontend/src/components/playground/modal/PenguinModal.tsx
@@ -6,6 +6,7 @@ import PokeModal from './PokeModal';
 import SendMessageModal from './SendMessageModal';
 import { getMoodMeaning } from '@/utils/getMoodMeaning';
 import { moodType } from '@/types/model';
+import send from '@/assets/icons/send.png';
 
 const PenguinModal = () => {
   const { modalClose, familyInfo } = playgroundPenguinStore();
@@ -23,20 +24,17 @@ const PenguinModal = () => {
             width="w-[18rem]"
           />
         </div>
-        <div className="w-full">
-          <div className="w-full h-full px-5 py-3 flex justify-between rounded-2xl shadow-xl text-left bg-white">
+        <div className="w-full flex justify-center">
+          <div className="w-4/5 h-full px-5 py-3 flex gap-5 justify-between rounded-2xl shadow-xl text-left bg-white">
             <div className="flex flex-col gap-1 justify-center">
               <p className="text-lg font-bold">{familyInfo.nickname} 펭귄</p>
-              <p className="text-sm">{getMoodMeaning(familyInfo.mood as moodType)}</p>
-              <p className="text-xs text-black/50">{familyInfo.birthday}</p>
+              <div className="flex items-end gap-2">
+                <p className="text-sm">{getMoodMeaning(familyInfo.mood as moodType)}</p>
+                <p className="text-xs text-black/50">{familyInfo.birthday}</p>
+              </div>
             </div>
-            <div className="flex items-center">
-              <button
-                onClick={() => setIsSendMessageModalOpen(true)}
-                className="w-14 h-14 rounded-full text-xs bg-SUB2 text-black font-bold"
-              >
-                메시지
-              </button>
+            <div className="w-12 h-12 rounded-full bg-slate-100 border">
+              <img src={send} className="" />
             </div>
           </div>
         </div>
