8cce8d21da1e2614136b1672c9b4bd8000a1da52
이희웅 <dlgmldnd017@naver.com>
2024-05-10 15:57:40 +0900

    [FE] feat #145: SPLASH UI 구현 및 (/) -> (/home) 경로 수정

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -5,19 +5,27 @@ import { useState } from 'react';
 import home from '@/assets/background_home.png';
 // import userStore from '@/stores/userStore';
 
+import { motion } from 'framer-motion';
+
 const HomePage = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   // const { mood } = userStore();
 
   return (
-    <div className="relative w-full h-full">
+    <motion.div
+      className="relative w-full h-full"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
       <img src={home} className="w-full h-full" />
       <div className="absolute left-0 right-0 bottom-[18%] flex justify-center">
         <Penguin mood="ANGRY" width="w-[17rem]" onClick={() => setIsOpen(true)} />
       </div>
       <HomeHeader />
       {isOpen && <PenguinDecoBottomSheet setIsOpen={setIsOpen} />}
-    </div>
+    </motion.div>
   );
 };
 
--- frontend/src/pages/splash/SplashPage.tsx
+++ frontend/src/pages/splash/SplashPage.tsx
@@ -1,5 +1,31 @@
+/* components */
+import thinking from '@/assets/mood/thinking.gif';
+
+/* libraries */
+import { useEffect } from 'react';
+import { motion } from 'framer-motion';
+
 const SplashPage = () => {
-  return <div>스플래시입니다</div>;
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.replace('/home');
+    }, 3000);
+  }, []);
+
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="w-full h-screen flex felx-col justify-center items-center bg-PLAYGROUND">
+          <img src={thinking} />
+        </div>
+      </motion.div>
+    </>
+  );
 };
 
 export default SplashPage;
