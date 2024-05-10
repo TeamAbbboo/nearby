18c3b74a621587a9b36f7c42a30203cfad5e562f
이희웅 <dlgmldnd017@naver.com>
2024-05-10 12:24:58 +0900

    [FE] fix : 카톡 공유 링크 수정 ({우리주소}/api) -> ({우리주소})

--- frontend/src/components/familycode/Solo.tsx
+++ frontend/src/components/familycode/Solo.tsx
@@ -85,16 +85,16 @@ const Solo = () => {
           description: '우리 가족 그룹에 참여하시겠습니까?',
           imageUrl: 'https://abbboo-nearby.s3.ap-northeast-2.amazonaws.com/story/hold_on_letter.png', // S3 이미지?
           link: {
-            mobileWebUrl: `${import.meta.env.BASE_URL}/login?code=${familyCode}`,
-            webUrl: `${import.meta.env.BASE_URL}/login?code=${familyCode}`,
+            mobileWebUrl: `${import.meta.env.VITE_FRONT_URL}/login?code=${familyCode}`,
+            webUrl: `${import.meta.env.VITE_FRONT_URL}/login?code=${familyCode}`,
           },
         },
         buttons: [
           {
             title: '지금 가족 그룹에 참여하기',
             link: {
-              mobileWebUrl: `${import.meta.env.BASE_URL}/login?code=${familyCode}`,
-              webUrl: `${import.meta.env.BASE_URL}/login?code=${familyCode}`,
+              mobileWebUrl: `${import.meta.env.VITE_FRONT_URL}/login?code=${familyCode}`,
+              webUrl: `${import.meta.env.VITE_FRONT_URL}/login?code=${familyCode}`,
             },
           },
         ],
