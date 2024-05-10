8c8e1b3459bd4663b2071fdb8a94b287fb12b62a
윤희원 <gmldnjs427@naver.com>
2024-05-10 10:00:07 +0900

    [FE] fix #66: userStore 사용부분 제거

--- frontend/src/services/greenhouse/api.ts
+++ frontend/src/services/greenhouse/api.ts
@@ -1,17 +1,17 @@
-import { axiosCommonInstance } from '@/apis/axiosInstance';
+import { axiosWithCredentialInstance } from '@/apis/axiosInstance';
 import { APIResponse } from '@/types/model';
 import { IGetCurrentLevelRes } from '@/types/greenhouse';
 
 /*현재 레벨, 경험치 */
 export const getCurrentLevelReq = async (): Promise<APIResponse<IGetCurrentLevelRes>> => {
-  const { data } = await axiosCommonInstance.get('/level');
+  const { data } = await axiosWithCredentialInstance.get('/level');
   console.log(data);
   return data;
 };
 
 /* 레벨업 */
 export const patchLevelUpReq = async (level: number): Promise<APIResponse<void>> => {
-  const { data } = await axiosCommonInstance.patch('/level', {
+  const { data } = await axiosWithCredentialInstance.patch('/level', {
     level: level,
   });
   return data;
