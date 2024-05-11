083d9813d217dc4b8edcb91eb26f0ac6c47eae76
윤희원 <gmldnjs427@naver.com>
2024-05-11 21:04:08 +0900

    [FE] feat #98: 온실 경험치 조회 UI 구현

--- frontend/src/components/greenhouse/GreenhouseHeader.tsx
+++ frontend/src/components/greenhouse/GreenhouseHeader.tsx
@@ -1,9 +1,10 @@
 import { firework } from '@/utils/firework';
-import { Dispatch, SetStateAction } from 'react';
+import { Dispatch, SetStateAction, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import story from '@/assets/icons/story.png';
 import playground from '@/assets/icons/playground.png';
 import { useGreenhouse } from '@/hooks/greenhouse/useGreenhouse';
+import ExpHistoryItem from '@/components/greenhouse/ExpHistoryItem';
 
 interface IDandelionState {
   level: number; // 레벨
@@ -12,10 +13,25 @@ interface IDandelionState {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
+interface IExpHistoryProp {
+  historyLevel: number;
+  point: number;
+  content: string;
+  createAt: string;
+}
+
 const GreenhouseHeader = ({ level, currentExp, maxExp, setIsOpen }: IDandelionState) => {
   /*레벨업*/
   const { usePatchLevelUp } = useGreenhouse();
   const { mutate } = usePatchLevelUp();
+  const [isExpOpen, setIsExpOpen] = useState<boolean>(false);
+
+  const expData: IExpHistoryProp = {
+    historyLevel: 1,
+    point: 3,
+    content: '일일 접속 +1',
+    createAt: '2024-04-07 12:12:12',
+  };
 
   const navigate = useNavigate();
   const progressPercentage = (currentExp / maxExp) * 100 >= 100 ? 100 : (currentExp / maxExp) * 100;
@@ -26,7 +42,10 @@ const GreenhouseHeader = ({ level, currentExp, maxExp, setIsOpen }: IDandelionSt
 
   return (
     <div className="p-5">
-      <div className="w-full h-24 bg-white rounded-3xl shadow-xl flex flex-col justify-center gap-2 px-5">
+      <div
+        className="w-full h-24 bg-white rounded-3xl shadow-xl flex flex-col justify-center gap-2 px-5"
+        onClick={() => setIsExpOpen(!isExpOpen)}
+      >
         <div className="flex justify-between">
           <div className="flex items-center gap-2">
             <p className="font-semibold text-lg ">모은 아띠</p>
@@ -49,7 +68,16 @@ const GreenhouseHeader = ({ level, currentExp, maxExp, setIsOpen }: IDandelionSt
           <div className="h-full bg-MAIN1 rounded-3xl" style={{ width: `${progressPercentage}%` }}></div>
         </div>
       </div>
-      <div className="w-full flex flex-col items-end pt-5 gap-3">
+      {isExpOpen && (
+        <div className="-flex flex-col p-5 w-full h-60 bg-white rounded-3xl shadow-xl overflow-y-auto">
+          <ExpHistoryItem data={expData} />
+          <ExpHistoryItem data={expData} />
+          <ExpHistoryItem data={expData} />
+          <ExpHistoryItem data={expData} />
+          <ExpHistoryItem data={expData} />
+        </div>
+      )}
+      <div className={`w-full flex flex-col items-end pt-5 gap-3 ${isExpOpen ? 'invisible' : 'visible'}`}>
         <div onClick={() => navigate('/playground')} className="flex flex-col items-center">
           <img src={playground} width={44} />
           <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
