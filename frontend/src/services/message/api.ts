528c70a5d4c8919de5d72c3efea43acd19a30aad
이승현 <sh980812@naver.com>
2024-05-11 22:22:25 +0900

    [FE] feat #157: 안 읽은 메시지 로직 처리 및 api 데이터 통신 구현

--- frontend/src/services/message/api.ts
+++ frontend/src/services/message/api.ts
@@ -1,5 +1,5 @@
 import { axiosCommonInstance } from '@/apis/axiosInstance';
-import { IMessageSendReq, IReceivedMessageListRes, ISentMessageListRes } from '@/types/message';
+import { IMessageSendReq, IReceivedMessageItem, IReceivedMessageListRes, ISentMessageListRes } from '@/types/message';
 import { APIResponse } from '@/types/model';
 
 // 받은 메시지 조회 API
@@ -38,8 +38,25 @@ export const getSentMessageList = async ({
   return data;
 };
 
+// 메시지 전송 API
 export const postSendMessage = async (req: IMessageSendReq): Promise<APIResponse<string>> => {
   const { data } = await axiosCommonInstance.post(`/messages`, req);
+  console.log(data);
+  return data;
+};
+
+// 안 읽은 메시지 조회 API
+export const getUnreadMessage = async (): Promise<APIResponse<IReceivedMessageItem | null>> => {
+  const { data } = await axiosCommonInstance.get(`/messages/unread`);
+  console.log(data);
+  return data;
+};
+
+// 메시지 읽음 처리 API
+export const patchUnreadMessage = async (messageId: number): Promise<APIResponse<string>> => {
+  const { data } = await axiosCommonInstance.patch(`/messages`, {
+    messageId,
+  });
   console.log(data);
   return data;
 };
