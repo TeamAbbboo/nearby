6d81d7380353f11fb584b8ee2dbda3831c7b527d
이희웅 <dlgmldnd017@naver.com>
2024-05-11 11:09:44 +0900

    [FE] feat #145: 공통 Splash 구현 및 playground, greenhouse 수정

---
+++ frontend/src/components/@common/SplashScreen.tsx
@@ -0,0 +1,57 @@
+/* components */
+import normal from '@/assets/mood/normal.gif';
+import thinking from '@/assets/mood/thinking.gif';
+import cheering from '@/assets/mood/cheering.gif';
+
+/* libraries */
+import { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
+
+/* interface */
+interface ISplashProps {
+  text?: string;
+  routerPath?: string;
+  imgSrc?: string;
+}
+
+const SplashScreen = ({ text, routerPath, imgSrc }: ISplashProps) => {
+  const [dots, setDots] = useState('.');
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.replace(routerPath);
+    }, 2600);
+
+    const interval = setInterval(() => {
+      setDots(prevDots => {
+        if (prevDots === '.') return '..';
+        else if (prevDots === '..') return '...';
+        else return '.';
+      });
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5 }}
+        className={imgSrc === 'home' ? 'bg-HOME' : imgSrc === 'playground' ? 'bg-PLAYGROUND' : 'bg-GREENHOUSE'}
+      >
+        <div className="w-full h-screen flex flex-col justify-center items-center">
+          <div className="text-3xl font-bold">
+            {text}
+            {dots}
+          </div>
+          <img src={imgSrc === 'home' ? normal : imgSrc === 'playground' ? thinking : cheering} />
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default SplashScreen;
