f5d16cb6c2d175f5ee23f82b9ea6657535240501
류지윤 <jiyun3016@gmail.com>
2024-05-12 11:26:56 +0900

    [BE] fix #172: 매핑 url 오타 수정

--- backend/src/main/java/com/abbboo/backend/domain/decoration/controller/DecorationController.java
+++ backend/src/main/java/com/abbboo/backend/domain/decoration/controller/DecorationController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("decoration")
+@RequestMapping("/decoration")
 @RequiredArgsConstructor
 public class DecorationController {
 
