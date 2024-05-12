84adc48d972ff95786751348b3ad8ad9b75ed7ec
류지윤 <jiyun3016@gmail.com>
2024-05-13 01:26:02 +0900

    [BE] style #195: 주석 띄어쓰기 수정

--- backend/src/main/java/com/abbboo/backend/domain/story/dto/res/MonthlyStoryRes.java
+++ backend/src/main/java/com/abbboo/backend/domain/story/dto/res/MonthlyStoryRes.java
@@ -12,6 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 public class MonthlyStoryRes {
 
-    private String yearMonth;                            // 조회한 년도-월, YYYY-MM 형식
+    private String yearMonth;             // 조회한 년도-월, YYYY-MM 형식
     private List<DayDTO> days;            // 일자별 대표 소식 이미지
 }
