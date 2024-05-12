197248cc9306d1e045bc2d1cae6697e2d9cac14e
윤희원 <gmldnjs427@naver.com>
2024-05-13 00:37:24 +0900

    [FE] feat #175: 소식 반응 등록 API 통신 구현

--- frontend/src/components/story/SendReactModal.tsx
+++ frontend/src/components/story/SendReactModal.tsx
@@ -2,6 +2,9 @@ import { Dispatch, SetStateAction } from 'react';
 import Modal from '../@common/Modal';
 import PenguinExpression from '@/components/story/PenguinExpression';
 import { expressionType } from '@/types/model';
+import { useStory } from '@/hooks/story/useStory';
+import { IStoryExpressionReq } from '@/types/story';
+import { getExpressionMeaning } from '@/utils/getExpressionMeaning';
 
 interface ISendReactProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
@@ -11,8 +14,18 @@ interface ISendReactProps {
 const SendReactModal = ({ setIsOpen, storyId }: ISendReactProps) => {
   console.log('반응하기 storyId', storyId);
 
+  const { usePostStoryExpression } = useStory();
+  const { mutate } = usePostStoryExpression();
+
   const sendReaction = (expression: expressionType) => {
-    console.log(expression);
+    const req: IStoryExpressionReq = {
+      storyId: storyId,
+      expression: expression,
+    };
+
+    mutate(req);
+    alert(` ${getExpressionMeaning(expression)} 반응 등록이 되었습니다.`);
+    setIsOpen(false);
   };
 
   return (
@@ -21,13 +34,13 @@ const SendReactModal = ({ setIsOpen, storyId }: ISendReactProps) => {
         <p>반응하기</p>
         <div className="grid grid-cols-2">
           <PenguinExpression expression="LOL" onClick={() => sendReaction('LOL')} />
-          <PenguinExpression expression="SAD" />
-          <PenguinExpression expression="OOPS" />
-          <PenguinExpression expression="COOL" />
-          <PenguinExpression expression="LOVE" />
-          <PenguinExpression expression="PRETTY" />
-          <PenguinExpression expression="GOOD" />
-          <PenguinExpression expression="BEST" />
+          <PenguinExpression expression="SAD" onClick={() => sendReaction('SAD')} />
+          <PenguinExpression expression="OOPS" onClick={() => sendReaction('OOPS')} />
+          <PenguinExpression expression="COOL" onClick={() => sendReaction('COOL')} />
+          <PenguinExpression expression="LOVE" onClick={() => sendReaction('LOVE')} />
+          <PenguinExpression expression="PRETTY" onClick={() => sendReaction('PRETTY')} />
+          <PenguinExpression expression="GOOD" onClick={() => sendReaction('GOOD')} />
+          <PenguinExpression expression="BEST" onClick={() => sendReaction('BEST')} />
         </div>
       </div>
     </Modal>
--- frontend/src/mocks/api/handlers/storyHandlers.ts
+++ frontend/src/mocks/api/handlers/storyHandlers.ts
@@ -1,9 +1,15 @@
 import { HttpResponse, http } from 'msw';
-import { getDayStoryReq } from '../data/story';
+import { getDayStoryReq, postStoryExpressionReq } from '../data/story';
 
 export const storyHandlers = [
   http.get('/stories/day', () => {
     return HttpResponse.json(getDayStoryReq, {
+      status: 200,
+    });
+  }),
+
+  http.post('/stories/5/reactions', () => {
+    return HttpResponse.json(postStoryExpressionReq, {
       status: 200,
     });
   }),
