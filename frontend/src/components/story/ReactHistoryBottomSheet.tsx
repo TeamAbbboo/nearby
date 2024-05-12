5c73af1852ac6e3dc1e3995c4ff278318cf50363
윤희원 <gmldnjs427@naver.com>
2024-05-12 23:26:59 +0900

    [FE] feat #175: 일별 소식 조회 및 반응 조회 API 통신 구현

--- frontend/src/components/story/ReactHistoryBottomSheet.tsx
+++ frontend/src/components/story/ReactHistoryBottomSheet.tsx
@@ -1,14 +1,29 @@
 import { Dispatch, SetStateAction } from 'react';
 import BottomSheet from '@/components/@common/BottomSheet';
+import ReactHistoryItem from './ReactHistoryItem';
+import { IReactionItem } from '@/types/story';
 
 interface IReactHistoryProps {
+  storyId: number;
+  reactionList?: IReactionItem[];
   setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
-const ReactHistoryBottomSheet = ({ setIsOpen }: IReactHistoryProps) => {
+const ReactHistoryBottomSheet = ({ setIsOpen, storyId, reactionList }: IReactHistoryProps) => {
+  console.log('반응 히스토리: storyId', storyId);
+
   return (
     <BottomSheet onClose={() => setIsOpen(false)}>
-      <div>반응 히스토리</div>
+      <div className="w-full h-80 overflow-auto">
+        {reactionList && reactionList.length > 0 ? (
+          reactionList.map((value, index) => <ReactHistoryItem key={index} reactionItem={value} />)
+        ) : (
+          <div className="p-5 justify-center text-center">
+            <p>반응 내역이 없습니다.</p>
+            <p>가장 먼저 반응을 남겨보세요!</p>
+          </div>
+        )}
+      </div>
     </BottomSheet>
   );
 };
--- frontend/src/components/story/SendReactModal.tsx
+++ frontend/src/components/story/SendReactModal.tsx
@@ -1,23 +1,33 @@
 import { Dispatch, SetStateAction } from 'react';
 import Modal from '../@common/Modal';
-import Penguin from '../@common/Penguin';
+import PenguinExpression from '@/components/story/PenguinExpression';
+import { expressionType } from '@/types/model';
 
 interface ISendReactProps {
   setIsOpen: Dispatch<SetStateAction<boolean>>;
+  storyId: number;
 }
 
-const SendReactModal = ({ setIsOpen }: ISendReactProps) => {
+const SendReactModal = ({ setIsOpen, storyId }: ISendReactProps) => {
+  console.log('반응하기 storyId', storyId);
+
+  const sendReaction = (expression: expressionType) => {
+    console.log(expression);
+  };
+
   return (
     <Modal onClose={() => setIsOpen(false)} width="w-4/5">
       <div className="p-5 flex flex-col bg-white w-full h-fit rounded-2xl justify-center font-bold text-center">
         <p>반응하기</p>
-        <div className="grid grid-cols-3">
-          <Penguin mood="ANGRY" />
-          <Penguin mood="CHEERUP" />
-          <Penguin mood="PASSION" />
-          <Penguin mood="SAD" />
-          <Penguin mood="TIRED" />
-          <Penguin mood="WORRY" />
+        <div className="grid grid-cols-2">
+          <PenguinExpression expression="LOL" onClick={() => sendReaction('LOL')} />
+          <PenguinExpression expression="SAD" />
+          <PenguinExpression expression="OOPS" />
+          <PenguinExpression expression="COOL" />
+          <PenguinExpression expression="LOVE" />
+          <PenguinExpression expression="PRETTY" />
+          <PenguinExpression expression="GOOD" />
+          <PenguinExpression expression="BEST" />
         </div>
       </div>
     </Modal>
