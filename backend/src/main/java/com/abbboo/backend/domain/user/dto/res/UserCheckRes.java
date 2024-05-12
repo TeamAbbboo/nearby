faf029619982e38cdfb7f03d67d9dbb83b370db2
신영한 <syhan7516@naver.com>
2024-05-12 22:30:46 +0900

    [BE] fix #187: 유저 정보 조회 응답 Dto 추가, 수정
    - 추가 내용 : 상태, 꾸미기 아이템 정보
    - 추가 이유 : 기존 유저 조회 응답 데이터가 매우 부족

--- backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserCheckRes.java
+++ backend/src/main/java/com/abbboo/backend/domain/user/dto/res/UserCheckRes.java
@@ -13,10 +13,18 @@ public class UserCheckRes {
 
     // 유저 생일
     private LocalDate birthday;
+    
+    // 유저 펭귄 상태
+    private String mood;
+
+    // 유저 펭귄 꾸미기 아이템
+    private String decoration;
 
     @Builder
-    public UserCheckRes(String nickname, LocalDate birthday) {
+    public UserCheckRes(String nickname, LocalDate birthday, String mood, String decoration) {
         this.nickname = nickname;
         this.birthday = birthday;
+        this.mood = mood;
+        this.decoration = decoration;
     }
 }
