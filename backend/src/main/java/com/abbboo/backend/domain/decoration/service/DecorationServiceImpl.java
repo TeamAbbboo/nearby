f993e7fafa23f7840a95af331e6d2ae4295df047
류지윤 <jiyun3016@gmail.com>
2024-05-12 22:22:47 +0900

    [BE] refactor #170: optional을 사용하여 효율적인 코드 작성

--- backend/src/main/java/com/abbboo/backend/domain/decoration/service/DecorationServiceImpl.java
+++ backend/src/main/java/com/abbboo/backend/domain/decoration/service/DecorationServiceImpl.java
@@ -35,10 +35,8 @@ public class DecorationServiceImpl implements DecorationService{
 
         // 요청한 아이템 정보 가져오기
         log.info("decoration 아이템 찾기");
-        Decoration decoration = decorationRepository.findByItemAndIsDeletedFalse(item);
-        if (decoration == null){
-            throw new NotFoundException(ErrorCode.ITEM_IS_WRONG);
-        }
+        Decoration decoration = decorationRepository.findByItemAndIsDeletedFalse(item)
+            .orElseThrow(() -> new NotFoundException(ErrorCode.ITEM_IS_WRONG));
         log.info("decoration 아이템 찾기 성공!");
 
         // 사용자 정보 업데이트
