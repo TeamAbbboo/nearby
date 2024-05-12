da67440827473bb633749623e582acdc8709a74b
류지윤 <jiyun3016@gmail.com>
2024-05-13 00:27:33 +0900

    [BE] fix #192: 메시지 조회 시 내림차순으로 수정

--- backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
+++ backend/src/main/java/com/abbboo/backend/domain/message/repository/MessageRepository.java
@@ -15,7 +15,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     // 가족들에게 보낸 메시지 조회
     @Query("select new com.abbboo.backend.domain.message.dto.res.SentMessageRes(" +
             "u.id, u.nickname, u.mood, m.content, m.createdAt)" +
-            "from User u join Message m on u.id = m.receiver.id " +
+            "from Message m join User u on u.id = m.receiver.id " +
             "where m.sender.id= :senderId and u.family.id= :familyId")
     Slice<SentMessageRes> findSentMessage(@Param("senderId") int senderId,
         @Param("familyId") int familyId, PageRequest pageRequest);
@@ -23,7 +23,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     // 가족들에게 받은 메시지 조회
     @Query("select new com.abbboo.backend.domain.message.dto.res.ReceivedMessageRes(" +
             "m.id, u.id, u.nickname, u.mood, m.content, m.createdAt, m.isRead, m.ttsUrl)" +
-            "from User u join Message m on u.id = m.sender.id " +
+            "from Message m join User u on u.id = m.sender.id " +
             "where m.receiver.id= :receiverId and u.family.id= :familyId")
     Slice<ReceivedMessageRes> findReceivedMessage(@Param("receiverId") int receiverId,
         @Param("familyId") int familyId, PageRequest pageRequest);
@@ -31,7 +31,7 @@ public interface MessageRepository extends JpaRepository<Message, Long> {
     // 읽지 않은 메시지 중 가장 최근 메시지 조회
     @Query("select new com.abbboo.backend.domain.message.dto.res.ReceivedMessageRes(" +
         "m.id,u.id, u.nickname, u.mood, m.content, m.createdAt, m.isRead, m.ttsUrl)" +
-        "from User u join Message m on u.id = m.sender.id " +
+        "from Message m join User u on u.id = m.sender.id " +
         "where m.receiver.id= :receiverId and u.family.id= :familyId and m.isRead = false " +
         "order by m.createdAt limit 1")
     ReceivedMessageRes findUnreadMessage(@Param("receiverId") int receiverId,
