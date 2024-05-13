a221a13cb7c4f07a5a082021fb14b27d4d81880c
윤희원 <gmldnjs427@naver.com>
2024-05-14 06:40:14 +0900

    [FE] refactor #142: 불필요한 컴포넌트 제거

--- frontend/src/pages/playground/PlaygroundPage.tsx
+++ frontend/src/pages/playground/PlaygroundPage.tsx
@@ -3,28 +3,23 @@ import PlaygroundHeader from '@/components/playground/PlaygroundHeader';
 import PLAYGROUND from '@/assets/background_playground.jpg';
 import { usePlayground } from '@/hooks/playground/usePlayground';
 import { motion } from 'framer-motion';
-import { ModalProvider } from '@/components/story/ModalContext';
-import ModalContent from '@/components/story/ModalContent';
 
 const PlaygroundPage = () => {
   const { useGetFamilyInfoList } = usePlayground();
   const { data: familyInfo } = useGetFamilyInfoList();
 
   return (
-    <ModalProvider>
-      <motion.div
-        className="relative w-full h-full font-NPSfontBold"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <img src={PLAYGROUND} className="w-full h-full" />
-        {familyInfo && <PenguinFamily familyInfo={familyInfo.data} />}
-        <PlaygroundHeader />
-        <ModalContent />
-      </motion.div>
-    </ModalProvider>
+    <motion.div
+      className="relative w-full h-full font-NPSfontBold"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img src={PLAYGROUND} className="w-full h-full" />
+      {familyInfo && <PenguinFamily familyInfo={familyInfo.data} />}
+      <PlaygroundHeader />
+    </motion.div>
   );
 };
 
