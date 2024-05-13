b58142fef19910093f3000d7850b9fa1b78484e6
윤희원 <gmldnjs427@naver.com>
2024-05-14 06:40:14 +0900

    [FE] refactor #142: 불필요한 컴포넌트 제거

--- frontend/src/pages/greenhouse/AlbumPage.tsx
+++ frontend/src/pages/greenhouse/AlbumPage.tsx
@@ -1,7 +1,5 @@
 import Calender from '@/components/greenhouse/Calender';
 import CalenderHeader from '@/components/greenhouse/CalenderHeader';
-import { ModalProvider } from '@/components/story/ModalContext';
-import ModalContent from '@/components/story/ModalContent';
 import { useLocation } from 'react-router-dom';
 
 interface RouteState {
@@ -14,15 +12,12 @@ const AlbumPage = () => {
   const { state: startDate } = useLocation() as RouteState;
   return (
     <>
-      <ModalProvider>
-        <CalenderHeader />
-        <div className="relative w-full h-full bg-GREENHOUSE bg-cover bg-center font-NPSfontBold">
-          <div className={`flex flex-col h-full w-full backdrop-blur-md py-20`}>
-            <Calender startDate={startDate.startDate} />
-          </div>
+      <CalenderHeader />
+      <div className="relative w-full h-full bg-GREENHOUSE bg-cover bg-center font-NPSfontBold">
+        <div className={`flex flex-col h-full w-full backdrop-blur-md py-20`}>
+          <Calender startDate={startDate.startDate} />
         </div>
-        <ModalContent />
-      </ModalProvider>
+      </div>
     </>
   );
 };
