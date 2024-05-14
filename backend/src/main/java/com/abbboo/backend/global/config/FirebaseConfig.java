d33a19b853d83557f821cfda29e0e0000ff097a6
신영한 <syhan7516@naver.com>
2024-05-14 16:29:14 +0900

    [BE] feat #219: FirebaseConfig 클래스 파일 생성

---
+++ backend/src/main/java/com/abbboo/backend/global/config/FirebaseConfig.java
@@ -0,0 +1,51 @@
+package com.abbboo.backend.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    // Firebase 설정 경로
+    @Value("${spring.firebase.path}")
+    private String FirebaseJsonPath;
+
+    // Firebase Messaging 생성 메서드
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+
+        // Firebase App Instance 가져오기
+        FirebaseApp firebaseApp = getFirebaseApp();
+
+        // FirebaseMessaging Instance 반환
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    // Firebase App Instance 가져오기 메서드
+    private FirebaseApp getFirebaseApp() throws IOException {
+
+        // Firebase SDK Key 가져오기
+        FileInputStream serviceAccountToken =
+                new FileInputStream(FirebaseJsonPath);
+
+        // Firebase Options 설정
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountToken))
+                .build();
+
+        // Firebase App Instance 가져오기
+        FirebaseApp firebaseApp = FirebaseApp.getApps().isEmpty() ?
+                FirebaseApp.initializeApp(options) :
+                FirebaseApp.getInstance();
+
+        return firebaseApp;
+    }
+}
