7ad290a18efa1cb73cb4bc92e778d6fd89ceb87d
이희웅 <dlgmldnd017@naver.com>
2024-05-14 17:12:54 +0900

    [FE] fix : 로그인 리다이렉트 수정 및 가족 코드 조회 홈 화면 추가, 소식 등록 막기

--- frontend/src/components/home/HomeHeader.tsx
+++ frontend/src/components/home/HomeHeader.tsx
@@ -2,7 +2,7 @@ import MessageModal from './MessageModal';
 import SettingModal from './SettingModal';
 import EditInfoModal from './EditInfoModal';
 import EditFamilyModal from './EditFamilyModal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import playground from '@/assets/icons/playground.png';
 import message from '@/assets/icons/message.png';
@@ -10,8 +10,29 @@ import setting from '@/assets/icons/setting.png';
 import camera from '@/assets/icons/camera.png';
 import notification from '@/assets/icons/notification.png';
 import Toast from '../@common/Toast/Toast';
+import { useFamily } from '@/hooks/family/useFamily';
 
 const HomeHeader = () => {
+  /* 가족 코드 */
+  const [familyCode, setFamilyCode] = useState<string>('');
+
+  /* 가족 코드 조회 */
+  /** 가족 코드가 존재하는데 또 참여하는 것을 방지하는 용도 */
+  const { useGetFamilyCode } = useFamily();
+  const { data, error } = useGetFamilyCode();
+  useEffect(() => {
+    if (data) {
+      setFamilyCode(data.data.familyCode);
+    }
+    if (error) {
+      console.log('유저 정보 받아오기 실패 : ' + error);
+    }
+  }, [data, error]);
+
+  const onPreventClick = () => {
+    Toast.error('가족을 생성해주세요.');
+  };
+
   const navigate = useNavigate();
   const [isMessageModalOpen, setIsMessageModalOpen] = useState<boolean>(false);
 
@@ -42,7 +63,10 @@ const HomeHeader = () => {
       <nav className="w-full p-5 flex justify-between font-bold">
         {/* 왼쪽 네비바 */}
         <div className="flex flex-col gap-3">
-          <div onClick={() => navigate('/story')} className="flex flex-col items-center">
+          <div
+            onClick={familyCode !== null ? () => navigate('/story') : onPreventClick}
+            className="flex flex-col items-center"
+          >
             <img src={camera} width={44} />
             <div className="bg-black/60 text-white rounded-2xl text-center w-[51px] h-4 flex items-center justify-center">
               <p className="text-[9px]">소식 등록</p>
