81fb8f2dbbb6a1348caf2b8692015e8d7680240b
윤희원 <gmldnjs427@naver.com>
2024-05-14 09:34:32 +0900

    [FE] fix #142: 충돌 해결

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -3,31 +3,26 @@ import PlaygroundHeader from '@/components/playground/PlaygroundHeader';
 import PLAYGROUND from '@/assets/background_playground.jpg';
 import { usePlayground } from '@/hooks/playground/usePlayground';
 import { motion } from 'framer-motion';
-import { ModalProvider } from '@/components/story/ModalContext';
-import ModalContent from '@/components/story/ModalContent';
 
 const HomePage = () => {
   const { useGetFamilyInfoList } = usePlayground();
   const { data: familyInfo } = useGetFamilyInfoList();
 
   return (
-    <ModalProvider>
-      <motion.div
-        className="relative w-full h-full font-NPSfontBold"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0 }}
-        transition={{ duration: 0.5 }}
-      >
-        <img src={PLAYGROUND} className="w-full h-full absolute top-0 -z-10" />
-        <PlaygroundHeader />
-        {familyInfo && <PenguinFamily familyInfo={familyInfo.data} />}
-        <ModalContent />
-        <div className="absolute bottom-3 right-5 text-lg text-white">
-          <p>가까이</p>
-        </div>
-      </motion.div>
-    </ModalProvider>
+    <motion.div
+      className="relative w-full h-full font-NPSfontBold"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img src={PLAYGROUND} className="w-full h-full absolute top-0 -z-10" />
+      <PlaygroundHeader />
+      {familyInfo && <PenguinFamily familyInfo={familyInfo.data} />}
+      <div className="absolute bottom-3 right-5 text-lg text-white">
+        <p>가까이</p>
+      </div>
+    </motion.div>
   );
 };
 
