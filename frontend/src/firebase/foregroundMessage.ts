369d2747ab59ed8cccfd34963a936a889e89758a
이승현 <sh980812@naver.com>
2024-05-16 18:03:35 +0900

    [FE] feat #237: FCM 추가 로직 구현

--- frontend/src/firebase/foregroundMessage.ts
+++ frontend/src/firebase/foregroundMessage.ts
@@ -1,17 +1,64 @@
-// import { getMessaging, onMessage } from 'firebase/messaging';
-// import { app } from './initFirebase';
+import { getToken, getMessaging, onMessage } from 'firebase/messaging';
+import { initializeApp } from 'firebase/app';
+import Toast from '@/components/@common/Toast/Toast';
 
-// const messaging = getMessaging(app);
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FCM_API_KEY,
+  authDomain: 'nearby-1439d.firebaseapp.com',
+  projectId: 'nearby-1439d',
+  storageBucket: 'nearby-1439d.appspot.com',
+  messagingSenderId: '1067829623938',
+  appId: '1:1067829623938:web:496249741d1e8c5fecc89b',
+  measurementId: 'G-9WX05FPJB4',
+};
 
-// onMessage(messaging, payload => {
-//   console.log('알림 도착 ', payload);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-//   const notificationTitle = payload.notification?.title;
-//   const notificationOptions = {
-//     body: payload.notification?.body,
-//   };
+onMessage(messaging, payload => {
+  console.log('알림 도착 ', payload.data);
+  Toast.success(payload.data?.title);
 
-//   if (Notification.permission === 'granted') {
-//     new Notification(notificationTitle!, notificationOptions);
-//   }
-// });
+  const { body, title } = payload.data!;
+  const notificationOptions = {
+    body: body,
+  };
+
+  if (Notification.permission === 'granted') {
+    new Notification(title, notificationOptions);
+  }
+});
+
+async function requestPermission() {
+  try {
+    const permission = await Notification.requestPermission(); // 권한 설정
+    const messaging = getMessaging(app);
+    console.log(permission);
+    if (permission === 'granted') {
+      const token = await getToken(messaging, {
+        vapidKey: import.meta.env.VITE_FCM_VAPID_KEY,
+      });
+      if (token) {
+        console.log(token);
+        // sendTokenToServer(token); // (토큰을 서버로 전송하는 로직)
+      } else {
+        Toast.error('토큰 등록이 불가능 합니다. 생성하려면 권한을 허용해주세요');
+      }
+    } else if (permission === 'denied') {
+      Toast.error('web push 권한이 차단되었습니다. 알림을 사용하시려면 권한을 허용해주세요');
+    }
+  } catch (error) {
+    console.error('푸시 토큰 가져오는 중에 에러 발생', error);
+  }
+}
+
+const getFirebaseToken = async () => {
+  const token = await getToken(messaging, {
+    vapidKey: import.meta.env.VITE_FCM_VAPID_KEY,
+  });
+
+  return token;
+};
+
+export { requestPermission, getFirebaseToken };
