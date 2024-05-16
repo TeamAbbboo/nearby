56529ef6138eef827d7ce30733ff8c20012c7ea5
신영한 <syhan7516@naver.com>
2024-05-16 21:36:47 +0900

    [BE] feat #236: 꾸욱 누르기 비지니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationService.java
@@ -1,5 +1,9 @@
 package com.abbboo.backend.domain.notification.service;
 
+import com.abbboo.backend.domain.notification.dto.req.NotificationPokeActionReq;
+
 public interface NotificationService {
 
+    // 꾸욱 누르기
+    void createPokeAction(String kakaoId, NotificationPokeActionReq notificationPokeActionReq);
 }
--- backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationServiceImpl.java
+++ backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationServiceImpl.java
@@ -1,8 +1,54 @@
 package com.abbboo.backend.domain.notification.service;
 
+import com.abbboo.backend.domain.notification.dto.req.NotificationPokeActionReq;
+import com.abbboo.backend.domain.notification.dto.res.NotificationPokeActionRes;
+import com.abbboo.backend.domain.user.entity.User;
+import com.abbboo.backend.domain.user.repository.UserRepository;
+import com.abbboo.backend.global.error.ErrorCode;
+import com.abbboo.backend.global.error.exception.NotFoundException;
+import com.abbboo.backend.global.event.NotificationEvent;
+import com.abbboo.backend.global.event.NotificationEventType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
+@RequiredArgsConstructor
 public class NotificationServiceImpl implements NotificationService {
 
+    private final UserRepository userRepository;
+    private final ApplicationEventPublisher eventPublisher;
+
+    // 꾸욱 누르기
+    @Override
+    @Transactional
+    public void createPokeAction(String kakaoId, NotificationPokeActionReq notificationPokeActionReq) {
+
+        // 유저 조회
+        User sender = userRepository.findByKakaoId(kakaoId)
+                .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        // 상대 유저 조회
+        User receiver = userRepository.findById(notificationPokeActionReq.getReceiverId())
+                        .orElseThrow(() -> new NotFoundException(ErrorCode.USER_NOT_FOUND));
+
+        log.info("꾸욱 누른 유저 ID : {} -> 찌름 당한 유저 ID : {}",sender.getId(),receiver.getId());
+
+        // 꾸욱 누르기 이벤트 정보 객체 생성
+        NotificationPokeActionRes notificationPokeActionRes
+                = NotificationPokeActionRes.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .title(sender.getNickname()+"펭귄")
+                .notificationEventType(NotificationEventType.POKE)
+                .build();
+
+        log.info("꾸욱 누르기 - 이벤트 정보 객체 생성 : OK");
+
+        // 꾸욱 누르기 이벤트 알림 발생
+        eventPublisher.publishEvent(new NotificationEvent(this,notificationPokeActionRes));
+    }
 }
