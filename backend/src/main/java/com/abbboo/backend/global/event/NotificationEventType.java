c51bb20b3989c95090a4606136f95a5fd3be27f9
신영한 <syhan7516@naver.com>
2024-05-16 21:28:02 +0900

    [BE] feat #236: 알림 이벤트 종류에 '꾸욱 누르기' 추가

--- backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
+++ backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
@@ -7,8 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum NotificationEventType {
     
-    // 알림 이벤트 종류 열거
+    // 알림 이벤트 종류
+    POKE("\"꾸욱\" 당신의 소식을 궁금해하는 펭귄이 있어요!")
     ;
-    
+
     private final String message;
 }
