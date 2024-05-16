46e8bdcde89ff5e012c8f84ff040c006b8d574fb
신영한 <syhan7516@naver.com>
2024-05-16 21:34:38 +0900

    [BE] feat #236: 알림 이벤트 객체 & 알림 이벤트 리스너 정의

--- backend/src/main/java/com/abbboo/backend/global/event/NotificationEvent.java
+++ backend/src/main/java/com/abbboo/backend/global/event/NotificationEvent.java
@@ -1,11 +1,33 @@
 package com.abbboo.backend.global.event;
 
+import com.abbboo.backend.domain.notification.dto.res.NotificationPokeActionRes;
+import com.abbboo.backend.domain.user.entity.User;
+import lombok.Builder;
+import lombok.Getter;
 import org.springframework.context.ApplicationEvent;
 
 // 알림 이벤트 리스너로 발생될 알림 이벤트 객체
+@Getter
 public class NotificationEvent extends ApplicationEvent {
 
-    public NotificationEvent(Object source) {
+    // 보내는 유저
+    private User seder;
+
+    // 받는 유저
+    private User receiver;
+
+    // 알림 제목
+    private String title;
+    
+    // 알림 종류
+    private NotificationEventType notificationEventType;
+
+    @Builder
+    public NotificationEvent(Object source, NotificationPokeActionRes notificationPokeActionRes) {
         super(source);
+        this.seder = notificationPokeActionRes.getSender();
+        this.receiver = notificationPokeActionRes.getReceiver();
+        this.title = notificationPokeActionRes.getTitle();
+        this.notificationEventType = notificationPokeActionRes.getNotificationEventType();
     }
 }
