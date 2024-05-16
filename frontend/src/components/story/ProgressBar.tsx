f000796dc96755774dc6f88ed5ac8e629248dcbf
윤희원 <gmldnjs427@naver.com>
2024-05-16 21:05:18 +0900

    [FE] design #246: 소식 프로그래스 바 높이 좁게 수정

--- frontend/src/components/story/ProgressBar.tsx
+++ frontend/src/components/story/ProgressBar.tsx
@@ -4,8 +4,8 @@ interface IProgressState {
 
 const ProgressBar = ({ progress }: IProgressState) => {
   return (
-    <div className="w-full h-1 bg-white/30 rounded-lg ">
-      <div className="h-1 bg-white w-full rounded-lg" style={{ width: `${progress}%` }}></div>
+    <div className="w-full h-[2px] bg-white/30 rounded-lg ">
+      <div className="h-[2px] bg-white w-full rounded-lg" style={{ width: `${progress}%` }}></div>
     </div>
   );
 };
