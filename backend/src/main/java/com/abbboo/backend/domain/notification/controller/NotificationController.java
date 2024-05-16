7c3f353c54dd5b3105448ba798c53e4a459b5a77
신영한 <syhan7516@naver.com>
2024-05-16 21:37:42 +0900

    [BE] feat #236: 꾸욱 누르기 API 구현

--- backend/src/main/java/com/abbboo/backend/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/abbboo/backend/domain/notification/controller/NotificationController.java
@@ -1,6 +1,7 @@
 package com.abbboo.backend.domain.notification.controller;
 
-import com.abbboo.backend.domain.user.dto.res.UserCheckRes;
+import com.abbboo.backend.domain.notification.dto.req.NotificationPokeActionReq;
+import com.abbboo.backend.domain.notification.service.NotificationService;
 import com.abbboo.backend.global.auth.CustomOAuth2User;
 import com.abbboo.backend.global.base.BaseResponse;
 import com.abbboo.backend.global.base.SuccessCode;
@@ -10,7 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +23,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "notifications", description = "알림 API")
 public class NotificationController {
 
+    private final NotificationService notificationService;
+
+    @Operation(summary = "꾸욱 누르기")
+    @PostMapping("/poke")
+    public ResponseEntity<BaseResponse> pokeUserOne(
+            @RequestBody NotificationPokeActionReq notificationPokeActionReq,
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User) {
+
+        log.info("꾸욱 누르기 URL 맵핑 : OK");
+
+        log.info("꾸욱 누르기 : START");
+        notificationService.createPokeAction(customOAuth2User.getCreatedUserId(), notificationPokeActionReq);
+        log.info("꾸욱 누르기 : COMPLETE");
+
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.POKE_CREATE_SUCCESS));
+    }
 }
