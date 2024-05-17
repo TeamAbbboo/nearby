6cec46e9cfa2a31f32b2603a2542e2cd4c996748
신영한 <syhan7516@naver.com>
2024-05-17 22:51:33 +0900

    [BE] feat #262: 알림 이벤트 종류에 메시지 수신 추가

--- backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
+++ backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
@@ -9,7 +9,8 @@ public enum NotificationEventType {
     
     // 알림 이벤트 종류
     POKE("\"꾸욱\" 당신의 소식을 궁금해하는 펭귄이 있어요!"),
-    CREATE_STORY("\"짜잔!\" 가족 중 누가 소식을 등록했어요!! 보러 가볼까요?" )
+    CREATE_STORY("\"짜잔!\" 가족 중 누가 소식을 등록했어요!! 보러 가볼까요?" ),
+    CREATE_LETTER("\"띵동!\" 새로운 메시지가 도착했습니다!! ")
     ;
 
     private final String message;
