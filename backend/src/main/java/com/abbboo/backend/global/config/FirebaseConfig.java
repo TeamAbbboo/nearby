21c78183995b4dc8e3875b2d39468dbcbd6218db
신영한 <syhan7516@naver.com>
2024-05-17 12:42:02 +0900

    [BE] fix #hotfix: 리소스 위치의 Firebase 설정 파일 접근 가능하도록 코드 수정

--- backend/src/main/java/com/abbboo/backend/global/config/FirebaseConfig.java
+++ backend/src/main/java/com/abbboo/backend/global/config/FirebaseConfig.java
@@ -7,16 +7,17 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
     // Firebase 설정 경로
     @Value("${spring.firebase.path}")
-    private String FirebaseJsonPath;
+    private String firebaseJsonPath;
 
     // Firebase Messaging 생성 메서드
     @Bean
@@ -33,8 +34,8 @@ public class FirebaseConfig {
     private FirebaseApp getFirebaseApp() throws IOException {
 
         // Firebase SDK Key 가져오기
-        FileInputStream serviceAccountToken =
-                new FileInputStream(FirebaseJsonPath);
+        ClassPathResource resource = new ClassPathResource(firebaseJsonPath);
+        InputStream serviceAccountToken = resource.getInputStream();
 
         // Firebase Options 설정
         FirebaseOptions options = new FirebaseOptions.Builder()
