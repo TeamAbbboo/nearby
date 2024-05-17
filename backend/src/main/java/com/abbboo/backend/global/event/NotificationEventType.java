6a66ad56afa85515c0a249ce112be2f3fbc5281a
신영한 <syhan7516@naver.com>
2024-05-17 23:08:02 +0900

    [BE] fix #262: 알림 메세지 내용 변경

--- backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
+++ backend/src/main/java/com/abbboo/backend/global/event/NotificationEventType.java
@@ -9,8 +9,8 @@ public enum NotificationEventType {
     
     // 알림 이벤트 종류
     POKE("\"꾸욱\" 당신의 소식을 궁금해하는 펭귄이 있어요!"),
-    CREATE_STORY("\"짜잔!\" 가족 중 누가 소식을 등록했어요!! 보러 가볼까요?" ),
-    CREATE_LETTER("\"띵동!\" 새로운 메시지가 도착했습니다!! ")
+    CREATE_STORY("\"짜잔!\" 새로운 소식이 등록되었어요!! 보러 가볼까요?"),
+    CREATE_LETTER("\"띵동!\" 새로운 메시지가 도착했습니다!! \n마음함을 확인해보세요!")
     ;
 
     private final String message;
