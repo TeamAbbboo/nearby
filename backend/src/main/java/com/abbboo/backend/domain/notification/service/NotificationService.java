595c9a1cd2af0437b252be3a2880d495454f14f2
신영한 <syhan7516@naver.com>
2024-05-19 02:27:45 +0900

    [BE] feat #269: 전체 알림 내역 조회 비지니스 로직 구현

--- backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationService.java
+++ backend/src/main/java/com/abbboo/backend/domain/notification/service/NotificationService.java
@@ -1,8 +1,14 @@
 package com.abbboo.backend.domain.notification.service;
 
 import com.abbboo.backend.domain.notification.dto.req.NotificationPokeActionReq;
+import com.abbboo.backend.domain.notification.dto.res.ReceivedNotificationRes;
+import com.abbboo.backend.global.base.PagenationReq;
+import org.springframework.data.domain.Slice;
 
 public interface NotificationService {
+
+    // 전체 알림 내역 조회
+    Slice<ReceivedNotificationRes> findReceivedNotification(String kakaoId, PagenationReq pagenationReq);
 
     // 꾸욱 누르기
     void createPokeAction(String kakaoId, NotificationPokeActionReq notificationPokeActionReq);
