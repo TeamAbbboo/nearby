693df96b0e67ac397972e98ce55953f4be7664b5
신영한 <syhan7516@naver.com>
2024-05-19 02:28:31 +0900

    [BE] feat #269: 전체 알림 내역 조회 API 구현

--- backend/src/main/java/com/abbboo/backend/domain/notification/controller/NotificationController.java
+++ backend/src/main/java/com/abbboo/backend/domain/notification/controller/NotificationController.java
@@ -1,20 +1,21 @@
 package com.abbboo.backend.domain.notification.controller;
 
 import com.abbboo.backend.domain.notification.dto.req.NotificationPokeActionReq;
+import com.abbboo.backend.domain.notification.dto.res.ReceivedNotificationRes;
 import com.abbboo.backend.domain.notification.service.NotificationService;
 import com.abbboo.backend.global.auth.CustomOAuth2User;
 import com.abbboo.backend.global.base.BaseResponse;
+import com.abbboo.backend.global.base.PagenationReq;
 import com.abbboo.backend.global.base.SuccessCode;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Slice;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -24,6 +25,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class NotificationController {
 
     private final NotificationService notificationService;
+
+    @Operation(summary = "전체 알림 내역 조회")
+    @GetMapping("")
+    public ResponseEntity<BaseResponse> getReceivedNotification(
+            @AuthenticationPrincipal CustomOAuth2User customOAuth2User,
+            @ModelAttribute @ParameterObject PagenationReq pagenationReq) {
+
+        log.info("전체 알림 내역 조회 URL 맵핑 : OK");
+
+        log.info("전체 알림 내역 조회 : START");
+        Slice<ReceivedNotificationRes> receivedNotifications =
+                notificationService.findReceivedNotification(customOAuth2User.getCreatedUserId(), pagenationReq);
+        log.info("전체 알림 내역 조회 : COMPLETE");
+
+        return ResponseEntity.ok(BaseResponse.of(SuccessCode.GET_RECEIVED_NOTIFICATION_SUCCESS,receivedNotifications));
+    }
 
     @Operation(summary = "꾸욱 누르기")
     @PostMapping("/poke")
