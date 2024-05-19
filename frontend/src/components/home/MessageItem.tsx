3f02856fea2fb5a3321fc4e11888b642b1bd0369
윤희원 <gmldnjs427@naver.com>
2024-05-19 21:13:50 +0900

    [FE] design: 스피커 재생 시 표시 및 크기 변경

--- frontend/src/components/home/MessageItem.tsx
+++ frontend/src/components/home/MessageItem.tsx
@@ -1,7 +1,9 @@
 import { IReceivedMessageItem, ISentMessageItem } from '@/types/message';
 import Penguin from '../@common/Penguin';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { simpleDecoType } from '@/constants/penguinState';
+import speaker from '@/assets/speaker.png';
+import listening from '@/assets/speaker_listening.gif';
 
 interface IMessageItemProps {
   messageItem: ISentMessageItem | IReceivedMessageItem;
@@ -11,6 +13,7 @@ interface IMessageItemProps {
 const MessageItem = ({ messageItem, decoration }: IMessageItemProps) => {
   const audioRef = useRef<HTMLAudioElement>(null);
   const [play, setPlay] = useState<boolean>(false);
+  const [speakerImage, setSpeakerImage] = useState<string>(speaker);
   const { content, mood, nickname, createdAt } = messageItem;
   const tts = 'ttsUrl' in messageItem ? messageItem.ttsUrl : ''; // in 연산자를 활용하여 type guard
 
@@ -18,39 +21,47 @@ const MessageItem = ({ messageItem, decoration }: IMessageItemProps) => {
     if (audioRef.current) {
       if (play) {
         // 재생중이라면
+        setSpeakerImage(speaker);
         audioRef.current.pause();
         setPlay(false);
       } else {
         // 정지중이라면
+        setSpeakerImage(listening);
         audioRef.current.play();
         setPlay(true);
       }
     }
   };
 
+  useEffect(() => {
+    if (audioRef.current) {
+      const handleEnded = () => {
+        setSpeakerImage(speaker);
+      };
+      audioRef.current.addEventListener('ended', handleEnded);
+      return () => {
+        audioRef.current?.removeEventListener('ended', handleEnded);
+      };
+    }
+  }, [audioRef]);
+
   return (
     <div className="py-2 border-b">
-      <div className="flex items-center gap-3 ">
+      <div className="flex items-center gap-3">
         <div className="w-14 h-14 rounded-full bg-SUB2">
           <Penguin mood={mood} decoration={decoration} />
         </div>
-        <div className="flex-1">
-          <div className="w-full flex justify-between">
+        <div className="flex flex-row justify-between flex-grow">
+          <div className="w-full flex-col">
             <p>{nickname}</p>
-            {tts && (
-              <>
-                <audio src={tts} ref={audioRef} />
-                <img
-                  onClick={() => playTTSAudio()}
-                  src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Speaker%20High%20Volume.png"
-                  alt="Speaker High Volume"
-                  width="20"
-                  height="20"
-                />
-              </>
-            )}
+            <p className="text-xs pt-1 text-UNIMPORTANT">{createdAt}</p>
           </div>
-          <p className="text-xs pt-1 text-UNIMPORTANT">{createdAt}</p>
+          {tts && (
+            <div className="px-1">
+              <audio src={tts} ref={audioRef} />
+              <img onClick={() => playTTSAudio()} src={speakerImage} alt="Speaker High Volume" width="40" height="40" />
+            </div>
+          )}
         </div>
       </div>
       <p className="p-2 whitespace-pre-wrap">{content}</p>
